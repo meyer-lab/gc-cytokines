aeef42ddb0836f0366d86cf296e5e648ced3534c
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2017-09-19 15:39:27 -0700

    Building changes, detailed balance, and unit test setup (#2)
    * Some different assumptions for an initial model
    
    * Remove methods left over from FcgR
    
    * Update acknowledgements.
    
    * Change header formatting.
    
    * Add build framework.
    
    * Fix.
    
    * Fix.
    
    * Reduce size.
    
    * Duplicate in species for IL15.

--- Manuscript/Text/05_Acknowledgements.md
+++ Manuscript/Text/05_Acknowledgements.md
@@ -1,6 +1,6 @@
 # Acknowledgements
 
- **Competing financial interests:** The authors declare no competing financial interests.
+This work was supported by NIH DP5-OD019815 to A.S.M. **Competing financial interests:** The authors declare no competing financial interests.
 
 # Author contributions statement
 
--- model/model.py
+++ model/model.py
@@ -1,46 +1,48 @@
 import numpy as np
 
 
+def dy_dt(y, t, IL2, IL15, k4fwd, k5rev, k6rev, k10rev, k11rev):
+    IL2Ra = y[0]
+    IL2Rb = y[1]
+    gc = y[2]
+    IL2_IL2Ra = y[3]
+    IL2_IL2Rb = y[4]
+    IL2_gc = y[5]
+    IL2_IL2Ra_IL2Rb = y[6]
+    IL2_IL2Ra_gc = y[7]
+    IL2_IL2Rb_gc = y[8]
+    IL2_IL2Ra_IL2Rb_gc = y[9]
+    IL15Ra = y[10]
+    IL15Rb = y[11]
+    IL15_IL15Ra = y[12]
+    IL15_IL15Rb = y[13]
+    IL15_gc = y[14]
+    IL15_IL15Ra_IL15Rb = y[15]
+    IL15_IL15Ra_gc = y[16]
+    IL15_IL15Rb_gc = y[17]
+    IL15_IL15Ra_IL15Rb_gc = y[18]
 
-def dy_dt(y, t, IL2, k1fwd, k4fwd, k4rev, k5rev, k8rev, k11rev):
-	IL2Ra = y[0]
-	IL2Rb = y[1]
-	gc = y[2]
-	IL2_IL2Ra = y[3]
-	IL2_IL2Rb = y[4]
-	IL2_gc = y[5]
-	IL2_IL2Ra_IL2Rb = y[6]
-	IL2_IL2Ra_gc = y[7]
-	IL2_IL2Rb_gc = y[8]
-	IL2_IL2Ra_IL2Rb_gc = y[9]
 
-	# The receptor-receptor forward rate is largely going to be determined by plasma membrane diffusion
-	# so we're going to assume it's shared.
-	k5fwd = k4fwd
-	k6fwd = k4fwd
-	k7fwd = k4fwd
-	k8fwd = k4fwd
-	k9fwd = k4fwd
-	k10fwd = k4fwd
-	k11fwd = k4fwd
-	k12fwd = k4fwd
+    # The receptor-receptor forward rate is largely going to be determined by plasma membrane diffusion
+    # so we're going to assume it's shared.
+    k5fwd = k6fwd = k7fwd = k8fwd = k9fwd = k10fwd = k11fwd = k12fwd = k4fwd
 
-	# To satisfy detailed balance these relationships should hold
-	# TODO: Will be specified later
-	k10rev = k8rev * 0 * 0
-	k9rev = k8rev * 0 * 0
+    # These are probably measured in the literature
+    k1fwd = 0
+    k1rev = 0
+    k2fwd = 0
+    k2rev = 0
+    k3fwd = 0
+    k3rev = 0
 
-	k6rev = k4rev * 0 * 0
-	k7rev = k5rev * 0 * 0
-	k12rev = k11rev * 0 * 0
-
-	# These are measured in the literature
-	k1rev = 0
-	k2fwd = 0
-	k2rev = 0
-	k3fwd = 0
-	k3rev = 0
-
+    # To satisfy detailed balance these relationships should hold
+    # _Based on initial assembly steps
+    k4rev = k1fwd * k4fwd * k6rev * k3rev / k1rev / k6fwd / k3fwd
+    k7rev = k3fwd * k7fwd * k2rev * k5rev / k2fwd / k5fwd / k3rev
+    k12rev = k2fwd * k12fwd * k1rev * k11rev / k11fwd / k1fwd / k2rev
+    # _Based on formation of full complex
+    k9rev = k2rev * k10rev * k12rev / k2fwd / k10fwd / k12fwd / k3rev / k6rev * k3fwd * k6fwd * k9fwd
+    k8rev = k2rev * k10rev * k12rev / k2fwd / k10fwd / k12fwd / k7rev / k3rev * k3fwd * k7fwd * k8fwd
 
 	dydt = np.zeros(y.shape, dtype = np.float64)
 
