07907a0c762aab210b1384fb2a8e002de049be4a
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2017-10-06 22:29:42 -0700

    Odeint (#7) added test for equilibrium
    * Working odeint
    
    Solves for y state at t = 100000, then checks the gradient norm is almost zero.
    
    * equilibrium unit test with error
    
    error with importing the dy_dt function
    
    * Update model.py
    
    * Update model.py

--- ckine/tests/test_model.py
+++ ckine/tests/test_model.py
@@ -1,4 +1,7 @@
 import unittest
+from ..model import dy_dt
+import numpy as np
+from scipy.integrate import odeint
 
 
 class TestModel(unittest.TestCase):
@@ -7,3 +10,13 @@ class TestModel(unittest.TestCase):
 
     def test_nchoosek(self):
         print("Ran test")
+        
+    def test_equilibrium(self):
+        ts = np.array([0.0, 100000.0])
+        y0 = np.ones((10, ), dtype = np.float64)
+        args = (1., 1., 1., 1., 1., 1., 0.5) # these 7 float values represent the inputs IL2 through k11rev
+        y, fullout = odeint(dy_dt, y0, ts, args,
+                    full_output = True, mxstep = 5000)
+        z = np.linalg.norm(dy_dt(y[1, :], 0, *args)) # have the sum of squares for all the dy_dt values be z
+        self.assertTrue(z < 1E-5)
+        
