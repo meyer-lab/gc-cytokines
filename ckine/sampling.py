fb8b9ad17e2447143c1e256a89a1b0ced9566e13
adamcweiner <30814024+adamcweiner@users.noreply.github.com>
2017-10-20 15:26:46 -0700

    Final matrix produced
    Arguments that generated error messages were not assigned to the table so their output values are all 0. The arguments and output data were concatenated together in 'table'.

--- ckine/sampling.py
+++ ckine/sampling.py
@@ -13,7 +13,10 @@ mat = np.array(np.meshgrid(w,x,z)).T.reshape(-1, 3)
 #print (mat.shape[0]) # returns 1331 rows formed by this list
 ys = np.zeros((1331, 10))
 #print (mat[256,2])
-for ii in range (mat.shape[0]):
+for ii in range (mat.shape[0]): # iterates through every combination of the arguments
     args = (1., mat[ii,0], mat[ii,1], mat[ii,2] )
-    ys[ii,:] = odeint(dy_dt_IL2_wrapper, y0, ts, args, mxstep = 5000)[1,:] # will need to add full_output = True and make a conversion to a tuple (https://stackoverflow.com/questions/15260342/having-trouble-enabling-full-output-in-odeint) in order to get more details on the 3 error messages
-print (ys)
+    temp, d = odeint(dy_dt_IL2_wrapper, y0, ts, args, mxstep = 6000, full_output=True)
+    if d['message'] == "Integration successful.": # only assign values to ys if there isn't an error message; all errors will still be 0
+        ys[ii,:] = temp[1,:] 
+table = np.concatenate((mat, ys), 1) # puts the arguments to the left of the output data in a matrix
+print (table)
