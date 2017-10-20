8d8433c21493612f380ecc22003319d152bdb2d4
adamcweiner <30814024+adamcweiner@users.noreply.github.com>
2017-10-19 19:46:32 -0700

    Table constructed with odeint solutions
    Iterates through every combination for all 3 unknowns. Still need to handle 3 error messages by getting more information with the full_output option when calling odeint.

--- ckine/sampling.py
+++ ckine/sampling.py
@@ -8,14 +8,12 @@ from model import dy_dt_IL2_wrapper
 t = 50. # let's let the system run for 50 seconds
 ts = np.linspace(0.0, t, 2)
 y0 = np.array([1000.,1000.,1000.,0.,0.,0.,0.,0.,0.,0.])
-x = np.logspace(-5, 5, num=11)
-#print (x)
-row = 0
-ys = np.empty((11, 10))
-#ys[1,] = odeint(dy_dt_IL2_wrapper, y0, ts, args, mxstep = 5000)
-for ii, item in enumerate(x):
-    args = (1., item, 10**-5, 10**-5)
-    ys[ii,:] = odeint(dy_dt_IL2_wrapper, y0, ts, args, mxstep = 5000)[1,:]
+z = w = x = np.logspace(-5, 5, num=11) # creates a list with floats ranging from 10**-5 to 10**5
+mat = np.array(np.meshgrid(w,x,z)).T.reshape(-1, 3)
+#print (mat.shape[0]) # returns 1331 rows formed by this list
+ys = np.zeros((1331, 10))
+#print (mat[256,2])
+for ii in range (mat.shape[0]):
+    args = (1., mat[ii,0], mat[ii,1], mat[ii,2] )
+    ys[ii,:] = odeint(dy_dt_IL2_wrapper, y0, ts, args, mxstep = 5000)[1,:] # will need to add full_output = True and make a conversion to a tuple (https://stackoverflow.com/questions/15260342/having-trouble-enabling-full-output-in-odeint) in order to get more details on the 3 error messages
 print (ys)
-
-#combinations module will make a list of things to loop through... function called combinations will take a bunch of vectors and give you all the combinations of them combined
