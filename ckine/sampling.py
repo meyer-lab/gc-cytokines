12813e6e664adddd32ac3bffab48ffe80fcd988f
adamcweiner <30814024+adamcweiner@users.noreply.github.com>
2017-10-26 14:14:20 -0700

    PCA with scores plot
    loadings plot still needs to be added

--- ckine/sampling.py
+++ ckine/sampling.py
@@ -1,6 +1,8 @@
 import numpy as np
 from scipy.integrate import odeint
 from model import dy_dt_IL2_wrapper
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 # IL2 (third argument) will always be 1 nM and there will be 1000 of each receptor (first 3 elements of first argument (y))
 # run odeint of dy_dt_IL2_wrapper([1000.,1000.,1000.,0.,0.,0.,0.,0.,0.,0.], ts, 1.,x,w,z) where x,w,z range from 10**-5 to 10**5 with increments by 10x
@@ -19,5 +21,29 @@ for ii in range (mat.shape[0]): # iterates through every combination of the argu
     if d['message'] == "Integration successful.": # only assign values to ys if there isn't an error message; all errors will still be 0
         ys[ii,:] = temp[1,:] 
 table = np.concatenate((mat, ys), 1) # puts the arguments to the left of the output data in a matrix
-print (table)
+#print (table)
 # fixed #18 
+
+# now going to use PCA to make sense of the data
+pca = PCA(n_components=3) # found percent of explained variance for each of the 13 components and the first 3 components each made up 33% of the explained variance
+scores = pca.fit_transform(table) # this has my scores - now need to print/plot it
+######## the following 5 lines found the % explained variance for each PC ############
+#exp_var = pca.explained_variance_ # this gives us the length of each eigenvalue in descending order
+#total_exp_var = sum(exp_var)
+#percent_exp_var = (exp_var * 100.) / total_exp_var
+#print (percent_exp_var)
+#print (sum(percent_exp_var))
+#print (pca.components_) # this gives me the scaling for the loadings plot
+#print (scores[:,0])
+
+plt.rcParams.update({'font.size': 8})
+plt.xlabel("PC 1")
+plt.ylabel("PC 2")
+plt.scatter(scores[:,0], scores[:,1])
+plt.show()
+
+plt.rcParams.update({'font.size': 8})
+plt.xlabel("PC 1")
+plt.ylabel("PC 3")
+plt.scatter(scores[:,0], scores[:,2])
+plt.show()
