ac05325366662c57ec04ffd840e3aef964370427
alifarhat30 <32119344+alifarhat30@users.noreply.github.com>
2017-11-06 15:29:57 -0800

    sampling IL15

---
+++ ckine/IL15_Sampling.py
@@ -0,0 +1,6 @@
+import numpy as np
+from scipy.integrate import odeint
+from model import dy_dt_IL15_wrapper
+from sklearn.decomposition import PCA
+from sklearn import preprocessing
+import matplotlib.pyplot as plt
