c47254cc9be32c3cfc25c91414a4ed65429c8b19
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2017-11-14 08:19:28 -0800

    Update sampling.py

--- ckine/sampling.py
+++ ckine/sampling.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 
 # IL2 (third argument) will always be 1 nM and there will be 1000 of each receptor (first 3 elements of first argument (y))
 # run odeint of dy_dt_IL2_wrapper([1000.,1000.,1000.,0.,0.,0.,0.,0.,0.,0.], ts, 1.,x,w,z) where x,w,z range from 10**-5 to 10**5 with increments by 10x
-
 t = 50. # let's let the system run for 50 seconds
 ts = np.linspace(0.0, t, 2)
 y0 = np.array([1000.,1000.,1000.,0.,0.,0.,0.,0.,0.,0.])
