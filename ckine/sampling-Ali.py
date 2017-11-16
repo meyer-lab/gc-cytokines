7aa558dc43bc62bf5a789b8afa595bd97ac26604
alifarhat30 <32119344+alifarhat30@users.noreply.github.com>
2017-11-15 20:50:08 -0800

    don't need this

--- ckine/sampling-Ali.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-from scipy.integrate import odeint
-from model import dy_dt_IL2_wrapper
-from sklearn.decomposition import PCA
-from sklearn import preprocessing
-import matplotlib.pyplot as plt
-
