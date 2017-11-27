62fba5333150d0739d829ec3da0e5491b38c3f20
adamcweiner <30814024+adamcweiner@users.noreply.github.com>
2017-11-27 14:56:48 -0800

    Pymc3 model adam (#48)
    * getting started
    
    I need help
    
    * Error with model.py
    
    changed to normal distribution instead of chi squared
    
    * Fixed. Will message about changes here.
    
    * Simplified some of the code layout.
    
    * Added sampling to show how this works.
    
    * created class for loading data
    
    Not entirely sure if this is what you meant but here's my first attempt. It won't run on my computer due to an error with pymc3's handling of logspace in lines 99-101
    
    * Fixed handling with class-based data loading. Redecorated with as_op
    
    * added function to store data
    
    * Run odeint in parallel.
    
    * bump
    
    * added build_model class
    
    Should help with pickling. Not sure what to do with the plot on line 116.
    
    * fixed plot

--- ckine/fit.py
+++ ckine/fit.py
@@ -4,6 +4,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 import pandas as pds
+from theano.compile.ops import as_op
+import theano.tensor as T
+import pymc3 as pm
+import pickle as pk
+import bz2
+import concurrent.futures
+
+
+pool = concurrent.futures.ProcessPoolExecutor()
 
 # this just takes the output of odeint (y values) and determines pSTAT activity
 def IL2_pSTAT_activity(ys):
@@ -21,60 +30,91 @@ def IL2_activity_input(y0, t, IL2, k4fwd, k5rev, k6rev):
 
 # this takes all the desired IL2 values we want to test and gives us the maximum activity value
 # IL2 values pretty much ranged from 5 x 10**-4 to 500 nm with 8 points in between
-def IL2_activity_values(y0, t, k4fwd, k5rev, k6rev):
+@as_op(itypes=[T.dscalar, T.dscalar, T.dscalar], otypes=[T.dmatrix])
+def IL2_activity_values(k4fwd, k5rev, k6rev):
+    y0 = np.array([1000.,1000.,1000.,0.,0.,0.,0.,0.,0.,0.])
+    t = 50.
     IL2s = np.logspace(-3.3, 2.7, 8) # 8 log-spaced values between our two endpoints
-    activity = np.zeros(8)
-    table = np.zeros((8,2))
-    for ii in range (IL2s.shape[0]):
-        activity[ii] = IL2_activity_input(y0, t, IL2s[ii], k4fwd, k5rev, k6rev)
-        table[ii, 0] = IL2s[ii]
-        table[ii, 1] = activity[ii]
+    table = np.zeros((8, 2))
+    output = list()
+
+    for ii in range(len(IL2s)):
+        output.append(pool.submit(IL2_activity_input, y0, t, IL2s[ii], k4fwd, k5rev, k6rev))
+
+    for ii in range(len(IL2s)):
+        table[ii, 1] = output[ii].result()
+    
+    table[:, 0] = IL2s
+
     return table
 
 
-def IL2_percent_activity(y0, t, k4fwd, k5rev, k6rev):
-    values = IL2_activity_values(y0, t, k4fwd, k5rev, k6rev)
-    maximum = np.amax(values[:,1], 0) # find the max value in the second column for all rows
-    #print (maximum) 
-    new_table = np.zeros((8,2))
-    x = np.zeros((8,1))
-    for ii in range (values.shape[0]):
-        new_table[ii, 0] = values[ii, 0] # IL2 values in first column are the same
-        new_table[ii, 1] = 100. * values[ii, 1] / maximum # activity values in second column are converted to percents relative to maximum
-        x[ii] = math.log10(new_table[ii,0]) # changing the x values to the log10(nM) values that were in the published graph
+
+def IL2_percent_activity(k4fwd, k5rev, k6rev):
+    values = IL2_activity_values(k4fwd, k5rev, k6rev)
+    maximum = T.max(values[:,1], 0) # find the max value in the second column for all rows
+
+    new_table = T.stack((values[:, 0], 100. * values[:, 1] / maximum), axis=1) # IL2 values in first column are the same
+    # activity values in second column are converted to percents relative to maximum
+    
+    return new_table
+
+
+def plot_IL2_percent_activity(y0, t, k4fwd, k5rev, k6rev):
+    new_table = IL2_percent_activity(k4fwd, k5rev, k6rev)
+
+    x = math.log10(new_table[:, 0]) # changing the x values to the log10(nM) values that were in the published graph
+
     plt.rcParams.update({'font.size': 8})
     plt.xlabel("IL2 concentration (log(nm))")
     plt.ylabel("percent activation of pSTAT")
     plt.scatter(x[:], new_table[:,1])
     plt.show()
-    return new_table
 
 
-def IL2_sum_squared_distance(y0, t, k4fwd, k5rev, k6rev):
-    activity_table = IL2_percent_activity(y0, t, k4fwd, k5rev, k6rev) # generates output from percent activity function
-    data = pds.read_csv("data\IL2_IL15_extracted_data.csv") # imports csv file into pandas array
-    # the following tries to perform calculation using pandas arrays
-    #data.columns = ['IL2_conc','h9_2Ra-','IL2_2Ra-','IL15_2Ra-','IL15_15Ra_2Ra-', 'h9_2Ra+','IL2_2Ra+','IL15_2Ra+','IL15_15Ra_2Ra+' ] #change the names of the pandas columns in data
-    #print (data['IL2_2Ra-'])
-    #print (activity_table[:,1])
-    #pandas_activity = pds.DataFrame({'conc':[activity_table[:,0]], 'act':[activity_table[:,1]]}) # converts percent activity data into pandas file in order to allow for subtraction
-    #differences_Ra- = pandas_activity['act'].sub(data['IL2_2Ra-'])
-    #print (type(pandas_activity['IL2_2Ra-'])) # this line is generating an error
+
+
+class IL2_sum_squared_dist:
     
-    # trying to perform calculation using numpy arrays
-    numpy_data = data.as_matrix() #the IL2_IL2Ra- data is within the 3rd column (index 2)
-    #print (numpy_data[:,2])
-    #print (activity_table[:,1])
-    diff_data = np.zeros((8,1))
-    diff_data[:,0] = numpy_data[:,6] - activity_table[:,1] # second column represents IL2_IL2Ra+ data
-    #print (diff_data)
-    squared_data = diff_data**2
-    #print (squared_data)
-    sum_squared_dist = np.sum(squared_data, 0) # 0 indicates that we're summing on the 0 axis
-    #print (sum_squared_dist)
-    root_sum_squared_dist = sum_squared_dist**0.5
-    dist_final = np.asscalar(np.array([root_sum_squared_dist])) # convert the numpy array of one float to a scalar float
-    return dist_final
+    def load(self):
+        data = pds.read_csv("./data/IL2_IL15_extracted_data.csv") # imports csv file into pandas array
+        self.numpy_data = data.as_matrix() #the IL2_IL2Ra- data is within the 3rd column (index 2)
+        
+    def calc(self, k4fwd, k5rev, k6rev):
+        activity_table = IL2_percent_activity(k4fwd, k5rev, k6rev)
+        diff_data = self.numpy_data[:,6] - activity_table[:,1]
+        return np.squeeze(diff_data)
+        
+def store_data(class_name, fit_results):
+    pk.dump(class_name, bz2.BZ2File(fit_results + '.pkl', 'wb'))
 
+class build_model:
+    
+    def __init__(self):
+        self.dst = IL2_sum_squared_dist()
+        self.dst.load()
+    
+    def build(self):   
+        self.M = pm.Model()
+        
+        with self.M:
+            k4fwd = pm.Lognormal('k4fwd', mu=0, sd=3) # do we need to add a standard deviation? Yes, and they're all based on a lognormal scale
+            k5rev = pm.Lognormal('k5rev', mu=0, sd=3)
+            k6rev = pm.Lognormal('k6rev', mu=0, sd=3)
+            
+            Y = self.dst.calc(k4fwd, k5rev, k6rev)
+            
+            Y_obs = pm.Normal('fitD', mu=0, sd=T.std(Y), observed=Y)
+        
+        return Y_obs
+    
+    def sampling(self):
+        with self.M:
+            start = pm.find_MAP()
+            step = pm.Metropolis()
+            self.trace = pm.sample(5000, step, start=start) # original value should be 5 to shorten time
+            
+        
+_ = plt.hist(build_model.trace['k4fwd'],100) # no longer need the 'self' because I am executing this line outside of the class
 
-print (IL2_sum_squared_distance([1000.,1000.,1000.,0.,0.,0.,0.,0.,0.,0.], 50., 1., 1., 1.))
+store_data(build_model, "model_results")
