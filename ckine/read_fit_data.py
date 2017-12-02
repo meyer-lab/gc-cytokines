946a9e92493bfe2228a834f2a00c254f419b643d
adamcweiner <30814024+adamcweiner@users.noreply.github.com>
2017-12-02 14:22:02 -0800

    edited plot features

--- ckine/read_fit_data.py
+++ ckine/read_fit_data.py
@@ -1,9 +1,20 @@
 import matplotlib.pyplot as plt
 from fit import build_model
 import pymc3 as pm
+import numpy as np
 
 def generate_plot(sampling_data, rate):
-    plt.hist(sampling_data[rate], 100)
+#    plt.hist(sampling_data[rate], bins=np.logspace(-3.3, 2.7, 8))
+    plt.hist(sampling_data[rate], bins=np.logspace(-3.6, 3.0, 9))
+    plt.gca().set_xscale("log")
+    plt.tick_params(
+        axis='x',          # changes apply to the x-axis
+        which='minor',      # only minor ticks are affected, major ticks left alone
+        bottom='off',      # ticks along the bottom edge are off
+        top='off',         # ticks along the top edge are off
+        labelbottom='on') # labels along the bottom edge are on
+    plt.title(rate)
+    plt.xlabel('IL2 conc. [nM]')
     plt.show()
 
 M = build_model()
@@ -12,5 +23,5 @@ M.build()
 sampling_data = pm.backends.text.load("IL2_model_results", model=M.M)
 
 generate_plot(sampling_data, 'k4fwd')
-#generate_plot(sampling_data, 'k5rev')
-#generate_plot(sampling_data, 'k6rev')
+generate_plot(sampling_data, 'k5rev')
+generate_plot(sampling_data, 'k6rev')
