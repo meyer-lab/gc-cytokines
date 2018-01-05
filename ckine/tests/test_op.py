a08d0cd96fdf4c42dd37b2e48158f8095838d2b5
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2018-01-05 13:09:47 -0800

    Gradient polishing (#69)
    Fix unit tests, more stringent ODE solving

--- ckine/tests/test_op.py
+++ ckine/tests/test_op.py
@@ -12,6 +12,11 @@ class TestOp(unittest.TestCase):
 
         Opp = centralDiff(mdl, parallel=False)
 
-        XX = np.ones(11, dtype=np.float64)
+        XX = np.full(11, 0.5, dtype=np.float64)
 
         utt.verify_grad(Opp, [XX])
+
+        # Array we've had trouble with before.
+        #probb = np.array([9.6680343654, 0.993278390419, 1.17495422785, 0.028015050235, 0.0397793662447, 0.118670968956, 1.9433832377, 0.0407013089368, 10.16551218, 10.88407284, 24.38150978], dtype=np.float64)
+
+        #utt.verify_grad(Opp, [probb])
