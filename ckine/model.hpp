d5338185cd83cb384dac0d129faf04f5243ef34b
Aaron Meyer <a@ameyer.me>
2018-02-11 13:31:35 -0800

    Remove intermediate executable

--- ckine/model.hpp
+++
@@ -1,9 +0,0 @@
-#include <array>
-
-extern "C" int runCkine (double *, size_t, double *, double *, double *);
-
-
-struct ratesS {
-	std::array<double, 11> trafRates;
-	std::array<double, 17> rxn;
-};
