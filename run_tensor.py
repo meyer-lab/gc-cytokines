14d73d9e2f7c5b0fd9562324175f3041556b550b
alifarhat30 <32119344+alifarhat30@users.noreply.github.com>
2018-02-13 17:10:58 -0800

    Pickle dump tensor

--- run_tensor.py
+++ run_tensor.py
@@ -1,7 +1,13 @@
 """
 Run tensor generation_file.
 """
+import numpy as np
+import pickle
 from ckine.tensor_generation import findy, activity_surf_tot
 
-y_of_combinations = findy()
+y_of_combinations, mat = findy(3,3)
 values = activity_surf_tot(y_of_combinations)
+tens_concat = np.concatenate((mat,values), axis = 1)
+
+filehandler = open("Tensor_results", 'w')
+pickle.dump(tens_concat, filehandler)
