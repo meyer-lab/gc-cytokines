4cce26e06e5fe3bfcc532270bff51298e2ac5048
alifarhat30 <32119344+alifarhat30@users.noreply.github.com>
2018-02-13 17:10:58 -0800

    Pickle dump tensor

--- ckine/tensor_generation.py
+++ ckine/tensor_generation.py
@@ -7,12 +7,12 @@ from scipy.integrate import odeint
 from .model import solveAutocrine, fullModel, getTotalActiveCytokine, __active_species_IDX, surfaceReceptors, totalReceptors
 
 
-def findy():
-    """A function to find the different values of y at different timepoints and different initial conditions."""
+def findy(lig, exp):
+    """A function to find the different values of y at different timepoints and different initial conditions. Takes in how many ligand concentrations and expression rates to iterate over."""
     t = 60. * 4 # let's let the system run for 4 hours
     ts = np.linspace(0.0, t, 100) #generate 100 evenly spaced timepoints
-    IL2 = IL15 = IL7 = IL9 = np.logspace(-3, 3, num=3)
-    IL2Ra = IL2Rb = gc = IL15Ra = IL7Ra = IL9R = np.logspace(-3, 2, num=3)
+    IL2 = IL15 = IL7 = IL9 = np.logspace(-3, 3, num=lig)
+    IL2Ra = IL2Rb = gc = IL15Ra = IL7Ra = IL9R = np.logspace(-3, 2, num=exp)
     mat = np.array(np.meshgrid(IL2,IL15,IL7,IL9,IL2Ra, IL2Rb, gc, IL15Ra, IL7Ra, IL9R)).T.reshape(-1, 10)
     #print (mat.shape[0]) gives 1024 for the above values; Need to update according to choice
 
@@ -36,11 +36,11 @@ def findy():
         temp, d = odeint(ddfunc, y0, ts, mxstep=12000, full_output=True, rtol=1.0E-5, atol=1.0E-3)
         if d['message'] == "Integration successful.":
             y_of_combos[ii] = temp # only assign values to ys if there isn't an error message; all errors will still be 0
-    return y_of_combos
+    return y_of_combos, mat
 
 
 def activity_surface_total(yVec):
-    """This function return a vector of 16 elements where the activity of the 4 cytokines and amounts of surface and total receptors are included."""
+    """This function returns a vector of 16 elements where the activity of the 4 cytokines and amounts of surface and total receptors are included."""
     x = np.zeros(16)
     x[0],x[1],x[2],x[3] = getTotalActiveCytokine(0,yVec), getTotalActiveCytokine(1,yVec), getTotalActiveCytokine(2,yVec), getTotalActiveCytokine(3,yVec)
     x[4:10] = surfaceReceptors(yVec)
