f2a308a13436a2c0d10132977bce0c9a6e0e79ee
alifarhat30 <32119344+alifarhat30@users.noreply.github.com>
2018-02-15 17:17:04 -0800

    tensor

--- run_tensor.py
+++ run_tensor.py
@@ -1,13 +1,17 @@
 """
-Run tensor generation_file.
+Run tensor_generation file.
 """
-import numpy as np
+import os
 import pickle
 from ckine.tensor_generation import findy, activity_surf_tot
 
-y_of_combinations, mat = findy(3,3)
+y_of_combinations, mat = findy(2,2)
 values = activity_surf_tot(y_of_combinations)
-tens_concat = np.concatenate((mat,values), axis = 1)
+#tens_concat = np.concatenate((np.atleast_3d(mat),values), axis = 1)
 
-filehandler = open("Tensor_results", 'w')
-pickle.dump(tens_concat, filehandler)
+#filehandler = open("Tensor_results", 'w')
+#pickle.dump((mat,values), filehandler)
+
+filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), "./ckine/data/Tensor_results/Sampling.pickle")
+with open(filename, 'wb') as f:
+    pickle.dump((mat,values), f)
