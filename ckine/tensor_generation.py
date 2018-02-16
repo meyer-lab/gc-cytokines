0609626ed0fbf85957ac41300fac4384378244bf
alifarhat30 <32119344+alifarhat30@users.noreply.github.com>
2018-02-16 11:19:27 -0800

    Add storage code for tensor generation (#86)
    * run tensor
    
    * Pickle dump tensor
    
    * tensor
    
    * Update run_tensor.py
    
    * Update tensor_generation.py
    
    * Update Tensor_analysis.py

--- ckine/tensor_generation.py
+++ ckine/tensor_generation.py
@@ -2,20 +2,20 @@
 Generate a tensor for the different y-values that arise at different timepoints during the model and with various initial conditions. The initial conditions vary the concentrations of the ligands and the expression rates of the receptors to simulate different cell lines.
 
 Important Notes:
-    y_of_combos is a multidimentional matrix of size (length mesh x 100 timeponts x 56 values of y)
-    values is also a multidementinal matrix of size (length mesh x 100 x 16 values for cytokine activity, surface receptors amount, and total receptors amount)
+    y_of_combos is a multidimensional matrix of size (length mesh x 100 timeponts x 56 values of y)
+    values is also a multidimensional matrix of size (length mesh x 100 x 16 values for cytokine activity, surface receptors amount, and total receptors amount)
 """
 import numpy as np
 from tqdm import tqdm
 from .model import getTotalActiveCytokine, runCkine, surfaceReceptors, totalReceptors
 
 
-def findy():
-    """A function to find the different values of y at different timepoints and different initial conditions."""
+def findy(lig, exp):
+    """A function to find the different values of y at different timepoints and different initial conditions. Takes in how many ligand concentrations and expression rates to iterate over."""
     t = 60. * 4 # let's let the system run for 4 hours
-    ts = np.linspace(0.01, t, 100) #generate 100 evenly spaced timepoints
-    IL2 = IL15 = IL7 = IL9 = np.logspace(-3, 3, num=2)
-    IL2Ra = IL2Rb = gc = IL15Ra = IL7Ra = IL9R = np.logspace(-3, 2, num=2)
+    ts = np.linspace(0.0, t, 100) #generate 100 evenly spaced timepoints
+    IL2 = IL15 = IL7 = IL9 = np.logspace(-3, 3, num=lig)
+    IL2Ra = IL2Rb = gc = IL15Ra = IL7Ra = IL9R = np.logspace(-3, 2, num=exp)
     mat = np.array(np.meshgrid(IL2,IL15,IL7,IL9,IL2Ra, IL2Rb, gc, IL15Ra, IL7Ra, IL9R)).T.reshape(-1, 10)
     #print (mat.shape[0]) gives 1024 for the above values; Need to update according to choice
 
@@ -38,12 +38,12 @@ def findy():
 
         if retVal >= 0:
             y_of_combos[ii] = temp # only assign values to ys if there isn't an error message; all errors will still be 0
-            
-    return y_of_combos
+
+    return y_of_combos, mat
 
 
 def activity_surface_total(yVec):
-    """This function return a vector of 16 elements where the activity of the 4 cytokines and amounts of surface and total receptors are included."""
+    """This function returns a vector of 16 elements where the activity of the 4 cytokines and amounts of surface and total receptors are included."""
     x = np.zeros(16)
     x[0],x[1],x[2],x[3] = getTotalActiveCytokine(0,yVec), getTotalActiveCytokine(1,yVec), getTotalActiveCytokine(2,yVec), getTotalActiveCytokine(3,yVec)
     x[4:10] = surfaceReceptors(yVec)
@@ -58,5 +58,3 @@ def activity_surf_tot(y_of_combos):
         for j in range(100):
             values[i][j] = activity_surface_total(y_of_combos[i][j])
     return values
-
-#Actually Return the 16 values per timepoint per combination
