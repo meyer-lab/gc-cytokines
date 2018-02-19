ba848ace96b0c020033cb9478fdd1ec11ff98263
alifarhat30 <32119344+alifarhat30@users.noreply.github.com>
2018-02-19 04:15:07 -0800

    Checkpoint

--- ckine/Tensor_analysis.py
+++ ckine/Tensor_analysis.py
@@ -1,9 +1,18 @@
 """
-Analyze tensor
+Analyze tensor from Sampling.pickle
 """
+import os
+import pickle
 import tensorly as tl
 import numpy as np
-from tensorly.decomposition import tucker
-X = tl.tensor(np.arange(24).reshape((3, 4, 2)))
-core, factors = tucker(X, ranks=[3, 4, 2])
-full_tensor = tl.tucker_to_tensor(core, factors)
+from tensorly.decomposition import partial_tucker
+
+
+filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), "./data/Tensor_results/Sampling.pickle")
+with open(filename, 'rb') as file:
+    sample = pickle.load(file)
+
+mat, values = sample[0], sample[1]
+
+#X = tl.tensor(values)
+core1, factors1 = partial_tucker(values,[0,1,2], ranks=[1024, 100, 16])
