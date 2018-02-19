faa5ede71d6b7c1c791349733563a7d8d5a61320
alifarhat30 <32119344+alifarhat30@users.noreply.github.com>
2018-02-19 02:31:56 -0800

    update

--- ckine/Tensor_analysis.py
+++ ckine/Tensor_analysis.py
@@ -1,3 +1,9 @@
 """
 Analyze tensor
 """
+import tensorly as tl
+import numpy as np
+from tensorly.decomposition import tucker
+X = tl.tensor(np.arange(24).reshape((3, 4, 2)))
+core, factors = tucker(X, ranks=[3, 4, 2])
+full_tensor = tl.tucker_to_tensor(core, factors)
--- run_tensor.py
+++ run_tensor.py
@@ -7,10 +7,7 @@ from ckine.tensor_generation import findy, activity_surf_tot
 
 y_of_combinations, mat = findy(2,2)
 values = activity_surf_tot(y_of_combinations)
-#tens_concat = np.concatenate((np.atleast_3d(mat),values), axis = 1)
 
-#filehandler = open("Tensor_results", 'w')
-#pickle.dump((mat,values), filehandler)
 
 filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), "./ckine/data/Tensor_results/Sampling.pickle")
 with open(filename, 'wb') as f:
