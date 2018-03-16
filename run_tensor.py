8b4a1a990c5a26dcdd47fae1843152db186a7aea
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2018-03-16 09:41:22 -0700

    Update run_tensor.py

--- run_tensor.py
+++ run_tensor.py
@@ -9,7 +9,6 @@ from ckine.tensor_generation import findy, activity_surf_tot
 y_of_combinations, mat = findy(2,2)
 values = activity_surf_tot(y_of_combinations)
 
-
 filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), "./ckine/data/Tensor_results/Sampling.pickle")
 with open(filename, 'wb') as f:
     pickle.dump((mat,values), f)
