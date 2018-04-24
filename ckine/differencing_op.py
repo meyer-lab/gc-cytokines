d9482b4b50ace81fa53e679b453dfdd56037d661
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2018-04-24 13:15:13 -0700

    Add the outline of an Op for FSA of kinetics (#117)
    * Overall setup, but not sure jacobian is being passed out correctly.
    * Skipping kinetic test.

--- ckine/differencing_op.py
+++ ckine/differencing_op.py
@@ -76,7 +76,7 @@ class runCkineOp(Op):
         if retVal < 0:
             yOut[:] = -np.inf
 
-        outputs[0][0] = np.squeeze(yOut.copy())
+        outputs[0][0] = np.squeeze(yOut)
 
     def grad(self, inputs, g):
         """ Calculate the runCkineOp gradient. """
@@ -100,3 +100,52 @@ class runCkineOpDiff(Op):
             sensi[:, :] = -np.inf
 
         outputs[0][0] = np.squeeze(sensi)
+
+
+# TODO: Warning! This is not passing tests.
+class runCkineKineticOp(Op):
+    itypes = [dvector]
+    otypes = [dvector]
+
+    def __init__(self, ts, condense):
+        assert condense.size == 56
+
+        self.ts = ts
+        self.condense = condense
+
+    def infer_shape(self, node, i0_shapes):
+        assert len(i0_shapes) == 1
+        return [(self.ts.size, )]
+
+    def perform(self, node, inputs, outputs):
+        yOut, retVal = runCkineU(self.ts, inputs[0])
+
+        assert yOut.shape == (self.ts.size, 56)
+
+        if retVal < 0:
+            yOut[:] = -np.inf
+
+        outputs[0][0] = np.dot(yOut, self.condense)
+
+    def grad(self, inputs, g):
+        """ Calculate the runCkineOp gradient. """
+        return [dot(g[0], runCkineOpKineticDiff(self.ts, self.condense)(inputs[0]))]
+
+
+class runCkineOpKineticDiff(Op):
+    itypes = [dvector]
+    otypes = [dmatrix]
+
+    def __init__(self, ts, condense):
+        assert condense.size == 56
+
+        self.ts = ts
+        self.condense = condense
+
+    def perform(self, node, inputs, outputs):
+        _, retVal, sensi = runCkineSensi(self.ts, inputs[0])
+
+        if retVal < 0:
+            sensi[:, :] = -np.inf
+
+        outputs[0][0] = np.dot(np.transpose(sensi), self.condense)
