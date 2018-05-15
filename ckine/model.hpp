0a89661ace6f9e8b249516a09452e8d376872081
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2018-05-15 16:55:06 -0700

    Minor changes to make sure fitting doesn’t end up with wrong values (#131)
    * Minor tweaks
    
    * Some tweaks to try and make the fitting work more reliably.

--- ckine/model.hpp
+++ ckine/model.hpp
@@ -59,7 +59,7 @@ constexpr double k26rev = 50000 * kfbnd; // General assumption that cytokine doe
 constexpr double k30rev = 50000 * kfbnd; // General assumption that cytokine doesn't bind to free gc
 
 constexpr double abstolIn = 1E-2;
-constexpr double reltolIn = 1E-5;
+constexpr double reltolIn = 1E-3;
 constexpr double internalV = 623.0; // Same as that used in TAM model
 constexpr double internalFrac = 0.5; // Same as that used in TAM model
 
