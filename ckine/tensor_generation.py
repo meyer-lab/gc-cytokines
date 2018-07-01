f900069f15de80a5677823f2e2dab0698de86cc1
alifarhat30 <32119344+alifarhat30@users.noreply.github.com>
2018-07-01 15:40:09 -0700

    Linting (#151)
    * Run Jenkin pipeline
    
    * Batch Two
    
    * Batch 3
    
    * Normal
    
    * Added Settings
    
    * Fixed a few docstrings

--- ckine/tensor_generation.py
+++ ckine/tensor_generation.py
@@ -5,9 +5,9 @@ Important Notes:
     values is also a multidimensional matrix of size (length mesh x 1000 x 16 values for cytokine activity, surface receptors amount, and total receptors amount)
 """
 
-import numpy as np, pandas as pds
 import os
 from os.path import join
+import numpy as np, pandas as pds
 from tqdm import tqdm
 from .model import getTotalActiveCytokine, runCkineU, surfaceReceptors, totalReceptors
 
@@ -17,24 +17,24 @@ data = pds.read_csv(join(path, 'data/expr_table.csv')) # Every column in the dat
 def findy(lig, timelength = 1000):
     """A function to find the different values of y at different timepoints and different initial conditions. Takes in how many ligand concentrations and expression rates to iterate over."""
     #Receptor expression levels were determined from the following cells through ImmGen
-    #Expression Value Normalized by DESeq2, and we have 34 types of cells 
+    #Expression Value Normalized by DESeq2, and we have 34 types of cells
     #Load the data from csv file
     path = os.path.dirname(os.path.abspath(__file__))
     data = pds.read_csv(join(path, 'data/expr_table.csv')) # Every column in the data represents a specific cell
     numpy_data = data.values # returns data values in a numpy array
     cell_names = data.columns.values.tolist()[1::] #returns the cell names from the pandas dataframe (which came from csv)
-    
+
     #np.delete removes the first column of the data which only includes the name of the receptors (6x35 to 6x34)
     #['Il2ra' 'Il2rb' 'Il2rg' 'Il15ra' 'Il7r' 'Il9r'] in that order
-    data_numbers = np.delete(numpy_data,0,1) 
-    
+    data_numbers = np.delete(numpy_data,0,1)
+
     t = 60. * 4 # let's let the system run for 4 hours
     ts = np.linspace(0.0, t, timelength) #generate 1000 evenly spaced timepoints
     IL2 = IL15 = IL7 = IL9 = np.logspace(-3, 3, num=lig) #cytokine stimulation concentrations
     #Goal is to make one cell expresison levels by len(mat) for every cell
     mat = np.array(np.meshgrid(IL2,IL15,IL7,IL9)).T.reshape(-1,4)
     mats = np.tile(mat,(len(cell_names),1)) # Repeat the cytokine stimulations (mat) an X amount of times where X here is number of cells (34)
-    receptor_repeats = np.repeat(data_numbers.T,len(mat), 0) #Create an array that repeats the receptor expression levels 'len(mat)' times 
+    receptor_repeats = np.repeat(data_numbers.T,len(mat), 0) #Create an array that repeats the receptor expression levels 'len(mat)' times
     new_mat = np.concatenate((mats,receptor_repeats), axis = 1) #concatenate to obtain the new meshgrid
 
     #Preset a y_of_combos
