375d35e04aea2ef0360078e462e6392f052108ae
Aaron Meyer <a@ameyer.me>
2018-07-09 16:43:24 -0700

    Fix?

--- genFigures.py
+++ genFigures.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 
 import sys
-import warnings
 import matplotlib
 matplotlib.use('AGG')
-warnings.filterwarnings(action="ignore", module="scipy", message="^internal gelsd")
 
 fdir = './Manuscript/Figures/'
 
 
 if __name__ == '__main__':
-    nameOut = 'Figure' + sys.argv[1]
+    nameOut = 'figure' + sys.argv[1]
 
     exec('from ckine.figures import ' + nameOut)
     ff = eval(nameOut + '.makeFigure()')
