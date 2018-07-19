0ce89533fa4b10ed0f23ca821c93e2bc549a557a
adamcweiner <30814024+adamcweiner@users.noreply.github.com>
2018-07-18 17:35:41 -0700

    Fit results (#179)
    * add averages and stdev
    
    * plots look better but still weird
    
    * pstat curves now 25 points each
    
    * simulating for longer time generates more realistic results
    
    * get even better results for 100,000 minutes
    
    * caught small bug with IL15 pstat plot
    
    * switch to np.max
    
    * switch back to 500 min for weirder IL15 results
    
    * changed stdev of different measurements
    
    * endosomal activity half as strong as surface
    
    * rough outline of surface gc plot
    
    * trying to figure out errors
    
    * fitting results from tighter stdevs
    
    * first attempt at IL2Rb in figure1
    
    * potential tweak with ax
    
    * Update makefile
    
    * import numpy
    
    * revert to original ax
    
    * copy plotting code into figure1
    
    * import pyplot
    
    * trying to generate subplots
    
    * add 'set_' prefixes
    
    * take out 'self'
    
    * take out "show()"
    
    * create size variable
    
    * first attempt at bringing in pstat
    
    * minor tweaks
    
    * syntax errors
    
    * adding surf gc
    
    * merge surface IL2Rb and gc into one class
    
    * delete redundant surface gc function
    
    * first attempt at importing samping results
    
    * fix syntax of for loop
    
    * name "M" was not defined
    
    * go up two folders
    
    * try os.path
    
    * import os
    
    * include model now
    
    * temp
    
    * import samples function near complete
    
    * delete other unkVec now that import_samples() works
    
    * plots loop over multiple unkVecs
    
    * try showing legend only once
    
    * get rid of legend and scale up to 100 unkVecs
    
    * ramp up to all 500 samples
    
    * delete unneccessary functions and file
    
    * change shape of unkVec
    
    * change shape of rxn in fitting
    
    * change size of endosomal ligand in getTotalActive
    
    * have percents be max=100
    
    * change active species IDX from bool to float
    
    * reduce tolIn to 1E-6
    
    * change index of IL2Ra
    
    * revert tolIn to boost speed
    
    * new fitting results
    
    * include all 4 chains
    
    * include all chains for sortF too
    
    * bump
    
    * loosen prior stdev for reverse rxn rates
    
    * first attempt at violin plots
    
    * change ax and switch back to only 1 chain
    
    * try with pandas df instead
    
    * comment out other plots to avoid runtime errors
    
    * trouble with pandas df
    
    * finished violin plot of reverse rxnrates
    
    * done with violin plots (for now)
    
    * set unused rxnrates to 1
    
    * update normalization in surface IL2Rb plot calc
    
    * fix normalization for pstat and surf_gc as well
    
    * make t an input parameter for calculation
    
    * made cytokC an input parameter for calculation
    
    * revert to old fit.py to see if plots improve
    
    * change IL2Ra- indices
    
    * change priors a bit
    
    * condense plot_structure into 1 function
    
    * make all plots normalized to 100%
    
    * condense violin plot code
    
    * Remove chain changes.
    
    * Update fit.py
    
    * Update fit.py
    
    * Update fit.py
    
    * Update fit.py
    
    * Put back changes.
    
    * Temporarily suspend building Figure 1.
    
    * Update model.py

--- ckine/plot_model_prediction.py
+++ ckine/plot_model_prediction.py
@@ -1,96 +1,57 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from .model import getTotalActiveSpecies, runCkineU, getSurfaceIL2RbSpecies, nSpecies, nParams
+from .model import getTotalActiveSpecies, runCkineU, getSurfaceIL2RbSpecies, nSpecies, nParams, getSurfaceGCSpecies
 
 
 class surf_IL2Rb:
     '''Generate values to match the surface IL2Rb measurements used in fitting'''
     def __init__(self):
-        # times from experiment are hard-coded into this function
-        self.ts = np.array([0., 2., 5., 15., 30., 60., 90.])
-
         # import function returns from model.py
         self.IL2Rb_species_IDX = getSurfaceIL2RbSpecies()
 
-        # percentage value that is used in scaling output
-        self.y_max = 10
-
-    def singleCalc(self, unkVec, cytokine, conc):
+    def singleCalc(self, unkVec, cytokine, conc, t):
         """ Calculates the surface IL2Rb over time for one condition. """
         unkVec = unkVec.copy()
         unkVec[cytokine] = conc
 
-        returnn, retVal = runCkineU(self.ts, unkVec)
+        returnn, retVal = runCkineU(t, unkVec)
 
         assert retVal >= 0
 
         a = np.dot(returnn, self.IL2Rb_species_IDX)
 
-        return a / a[0]
+        return a
 
-    def calc(self, unkVec):
+    def calc(self, unkVec, t):
         '''This function uses an unkVec that has the same elements as the unkVec in fit.py'''
 
         assert unkVec.size == nParams()
 
         # set IL2 concentrations
         unkVecIL2RaMinus = unkVec.copy()
-        unkVecIL2RaMinus[18] = 0.
+        unkVecIL2RaMinus[22] = 0.
 
         # calculate IL2 stimulation
-        a = self.singleCalc(unkVec, 0, 1.)
-        b = self.singleCalc(unkVec, 0, 500.)
-        c = self.singleCalc(unkVecIL2RaMinus, 0, 1.)
-        d = self.singleCalc(unkVecIL2RaMinus, 0, 500.)
+        a = self.singleCalc(unkVec, 0, 1., t)
+        b = self.singleCalc(unkVec, 0, 500., t)
+        c = self.singleCalc(unkVecIL2RaMinus, 0, 1., t)
+        d = self.singleCalc(unkVecIL2RaMinus, 0, 500., t)
 
         # calculate IL15 stimulation
-        e = self.singleCalc(unkVec, 1, 1.)
-        f = self.singleCalc(unkVec, 1, 500.)
-        g = self.singleCalc(unkVecIL2RaMinus, 1, 1.)
-        h = self.singleCalc(unkVecIL2RaMinus, 1, 500.)
+        e = self.singleCalc(unkVec, 1, 1., t)
+        f = self.singleCalc(unkVec, 1, 500., t)
+        g = self.singleCalc(unkVecIL2RaMinus, 1, 1., t)
+        h = self.singleCalc(unkVecIL2RaMinus, 1, 500., t)
 
-        return np.concatenate((a, b, c, d, e, f, g, h))
-
-    def plot_structure(self, IL2vec, IL15vec, title):
-        plt.title(title)
-        plt.scatter(self.ts, IL2vec, color='r', label='IL2', alpha=0.7)
-        plt.scatter(self.ts, IL15vec, color='g', label='IL15', alpha=0.7)
-        # plt.ylim(0,(y_max + (0.2 * y_max)))
-        plt.ylabel("Surface IL2Rb (% x " + str(self.y_max) + ')')
-        plt.xlabel("Time (min)")
-        plt.legend()
-        plt.show()
-
-    def plot(self, unkVec):
-        output = self.calc(unkVec) * self.y_max
-        IL2_1_plus = output[0:7]
-        IL2_500_plus = output[7:14]
-        IL2_1_minus = output[14:21]
-        IL2_500_minus = output[21:28]
-        IL15_1_plus = output[28:35]
-        IL15_500_plus = output[35:42]
-        IL15_1_minus = output[42:49]
-        IL15_500_minus = output[49:56]
-
-        self.plot_structure(IL2_1_minus, IL15_1_minus, '1 nM and IL2Ra-')
-        self.plot_structure(IL2_500_minus, IL15_500_minus, "500 nM and IL2Ra-")
-        self.plot_structure(IL2_1_plus, IL15_1_plus, "1 nM and IL2Ra+")
-        self.plot_structure(IL2_500_plus, IL15_500_plus, "500 nM and IL2Ra+")
-
+        return (np.concatenate((a, b, c, d, e, f, g, h)) / a[0])
 
 class pstat:
     '''Generate values to match the pSTAT5 measurements used in fitting'''
     def __init__(self):
-        self.PTS = 30
-        self.cytokC = np.logspace(-3.3, 2.7, self.PTS) # 8 log-spaced values between our two endpoints
-        
         # import function returns from model.py
         self.activity = getTotalActiveSpecies().astype(np.float64)
         
         self.ts = np.array([500.]) # was 500. in literature
-
-        # percentage value that is used in scaling output
-        self.y_max = 100
 
     def singleCalc(self, unkVec, cytokine, conc):
         """ Calculates the surface IL2Rb over time for one condition. """
@@ -103,41 +64,62 @@ class pstat:
 
         return np.dot(returnn, self.activity)
 
-    def calc(self, unkVec):
+    def calc(self, unkVec, cytokC):
         '''This function uses an unkVec that has the same elements as the unkVec in fit.py'''
-
         assert unkVec.size == nParams()
 
         unkVec_IL2Raminus = unkVec.copy()
-        unkVec_IL2Raminus[18] = 0.0 # set IL2Ra expression rate to 0
+        unkVec_IL2Raminus[22] = 0.0 # set IL2Ra expression rate to 0
 
         # Calculate activities
-        actVec_IL2 = np.fromiter((self.singleCalc(unkVec, 0, x) for x in self.cytokC), np.float64)
-        actVec_IL2_IL2Raminus = np.fromiter((self.singleCalc(unkVec_IL2Raminus, 0, x) for x in self.cytokC), np.float64)
-        actVec_IL15 = np.fromiter((self.singleCalc(unkVec, 1, x) for x in self.cytokC), np.float64)
-        actVec_IL15_IL2Raminus = np.fromiter((self.singleCalc(unkVec_IL2Raminus, 1, x) for x in self.cytokC), np.float64)
+        actVec_IL2 = np.fromiter((self.singleCalc(unkVec, 0, x) for x in cytokC), np.float64)
+        actVec_IL2_IL2Raminus = np.fromiter((self.singleCalc(unkVec_IL2Raminus, 0, x) for x in cytokC), np.float64)
+        actVec_IL15 = np.fromiter((self.singleCalc(unkVec, 1, x) for x in cytokC), np.float64)
+        actVec_IL15_IL2Raminus = np.fromiter((self.singleCalc(unkVec_IL2Raminus, 1, x) for x in cytokC), np.float64)
 
         # Normalize to the maximal activity, put together into one vector
-        actVec = np.concatenate((actVec_IL2 / np.max(actVec_IL2), actVec_IL2_IL2Raminus / np.max(actVec_IL2_IL2Raminus), actVec_IL15 / np.max(actVec_IL15), actVec_IL15_IL2Raminus / np.max(actVec_IL15_IL2Raminus)))
+        actVec = np.concatenate((actVec_IL2, actVec_IL2_IL2Raminus, actVec_IL15, actVec_IL15_IL2Raminus))
 
-        return actVec
+        return (actVec / np.max(actVec))
 
-    def plot_structure(self, IL2vec, IL15vec, title):
-        plt.title(title)
-        plt.scatter(np.log10(self.cytokC), IL2vec, color='r', alpha=0.5, label="IL2")
-        plt.scatter(np.log10(self.cytokC), IL15vec, color='g', alpha=0.5, label='IL15')
-        plt.ylim(0,(self.y_max + (0.25*self.y_max)))
-        plt.ylabel('Maximal p-STAT5 (% x ' + str(self.y_max) + ')')
-        plt.xlabel('log10 of cytokine concentration (nM)')
-        plt.legend()
-        plt.show()
+class surf_gc:
+    def __init__(self):
+        # import function returns from model.py
+        self.gc_species_IDX = getSurfaceGCSpecies()
+        
+    def singleCalc(self, unkVec, cytokine, conc, t):
+        """ Calculates the surface gc over time for one condition. """
+        unkVec = unkVec.copy()
+        unkVec[cytokine] = conc
 
-    def plot(self, unkVec):
-        output = self.calc(unkVec) * self.y_max
-        IL2_plus = output[0:self.PTS]
-        IL2_minus = output[self.PTS:(self.PTS*2)]
-        IL15_plus = output[(self.PTS*2):(self.PTS*3)]
-        IL15_minus = output[(self.PTS*3):(self.PTS*4)]
+        returnn, retVal = runCkineU(t, unkVec)
 
-        self.plot_structure(IL2_minus, IL15_minus, "IL2Ra- YT-1 cells")
-        self.plot_structure(IL2_plus, IL15_plus, "IL2Ra+ YT-1 cells")
+        assert retVal >= 0
+
+        a = np.dot(returnn, self.gc_species_IDX)
+
+        return a
+    
+    def calc(self, unkVec, t):
+        '''This function calls single Calc for all the experimental combinations of interest; it uses an unkVec that has the same elements as the unkVec in fit.py'''
+
+        assert unkVec.size == nParams()
+
+        # set IL2 concentrations
+        unkVecIL2RaMinus = unkVec.copy()
+        unkVecIL2RaMinus[22] = 0.
+
+        # calculate IL2 stimulation
+        a = self.singleCalc(unkVec, 0, 1., t)
+        b = self.singleCalc(unkVec, 0, 500., t)
+        c = self.singleCalc(unkVecIL2RaMinus, 0, 1., t)
+        d = self.singleCalc(unkVecIL2RaMinus, 0, 500., t)
+
+        # calculate IL15 stimulation
+        e = self.singleCalc(unkVec, 1, 1., t)
+        f = self.singleCalc(unkVec, 1, 500., t)
+        g = self.singleCalc(unkVecIL2RaMinus, 1, 1., t)
+        h = self.singleCalc(unkVecIL2RaMinus, 1, 500., t)
+
+        return (np.concatenate((a, b, c, d, e, f, g, h)) / a[0])
+    
