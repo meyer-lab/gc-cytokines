3e69554e7f8ef1a5af0a19170920414e33aa12da
alifarhat30 <32119344+alifarhat30@users.noreply.github.com>
2018-07-21 10:00:49 -0700

    Calculate multiple R2X (#193)

--- ckine/figures/figure3.py
+++ ckine/figures/figure3.py
@@ -5,7 +5,7 @@ import string
 from .figureCommon import subplotLabel, getSetup
 import numpy as np
 from ..tensor_generation import prepare_tensor
-from ..Tensor_analysis import perform_decomposition, find_R2X
+from ..Tensor_analysis import perform_decomposition, find_R2X, split_R2X
 
 
 def makeFigure():
@@ -18,8 +18,17 @@ def makeFigure():
     ax[0].axis('off')
 
     values, _, _, _, cell_names = prepare_tensor(2,50)
-    plot_R2X(ax[3], values, n_comps = 17)
+    n_comps = 17
 
+    plot_R2X(ax[3], values, n_comps)
+    plot_split_R2X(ax[4], ax[5], ax[6], values, n_comps)
+    
+    for n in range(3,7):
+        ax[n].set_xticks(np.arange(1, n_comps+1))
+        ax[n].set_xticklabels(np.arange(1, n_comps+1))
+        ax[n].set_ylim(0, 1)
+        ax[n].set_xlabel('Number of Components')
+    
     # Add subplot labels
     for ii, item in enumerate(ax):
         subplotLabel(item, string.ascii_uppercase[ii])
@@ -35,8 +44,17 @@ def plot_R2X(ax, tensor, n_comps):
         R2X = find_R2X(tensor, n)
         R2X_array.append(R2X)
     ax.bar(range(1,n_comps+1), R2X_array)
-    ax.set_xticks(np.arange(1, n_comps+1))
-    ax.set_xticklabels(np.arange(1, n_comps+1))
-    ax.set_ylim(0, 1)
-    ax.set_xlabel('Number of Components')
     ax.set_ylabel('R2X')
+
+def plot_split_R2X(ax1, ax2, ax3, tensor, n_comps):
+    """This function takes in the values tensor, splits it up into a mini tensor corresponding to the splitType. If splitType =1, then split ligand activities, if =2, then split surface receptors, if = 3, then split total receptors."""
+    R2X_matrix = split_R2X(tensor, n_comps)
+
+    ax1.bar(range(1,n_comps+1), R2X_matrix[0,:])
+    ax1.set_ylabel('Ligand Activity R2X')
+
+    ax2.bar(range(1,n_comps+1), R2X_matrix[1,:])
+    ax2.set_ylabel('Surface Receptors R2X')
+
+    ax3.bar(range(1,n_comps+1), R2X_matrix[2,:])
+    ax3.set_ylabel('Total Receptors R2X')
