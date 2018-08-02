398a4184249db9c3dc6a3dbcb807e3a8b05f454c
adamcweiner <30814024+adamcweiner@users.noreply.github.com>
2018-08-02 12:01:43 -0700

    Figure layout (#218)
    * re-orient panels and legends
    
    * rotate axes labels
    
    * more edits
    
    * resize subplots and add legends
    
    * trying to make compareRates span 2 subplots
    
    * fixed default argument of empts
    
    * bring back all figures
    
    * edit labeling
    
    * convert pSTAT plots to confidence intervals
    
    * put confidence intervals for surface % data
    
    * set axes labels and titles
    
    * changed legends in figure1
    
    * add confidence intervals to IL4-7 activity
    
    * delete outdated function/code
    
    * pretreatment confidence intervals look funny
    
    * fixed typos with crosstalk plot
    
    * surface gc plot has confidence intervals
    
    * make plotting function in figureCommon
    
    * updated cross-talk plot
    
    * all plots use plot_conf_int function
    
    * condense code
    
    * color map kind of works

--- ckine/figures/figureCommon.py
+++ ckine/figures/figureCommon.py
@@ -3,8 +3,9 @@
 """
 import seaborn as sns
 from matplotlib import gridspec, pyplot as plt
+import numpy as np
 
-def getSetup(figsize, gridd):
+def getSetup(figsize, gridd, mults=None, multz=None, empts=[]):
     sns.set(style="whitegrid",
             font_scale=0.7,
             color_codes=True,
@@ -17,9 +18,13 @@ def getSetup(figsize, gridd):
 
     # Make grid
     gs1 = gridspec.GridSpec(*gridd)
-
+    
     # Get list of axis objects
-    ax = [f.add_subplot(gs1[x]) for x in range(gridd[0] * gridd[1])]
+    if mults is None:
+        ax = [f.add_subplot(gs1[x]) for x in range(gridd[0] * gridd[1])]
+    else:
+        ax = [f.add_subplot(gs1[x]) if x not in mults else f.add_subplot(gs1[x:x + multz[x]]) for x in range(
+            gridd[0] * gridd[1]) if not any([x - j in mults for j in range(1, max(multz.values()))]) and x not in empts]
 
     return (ax, f)
 
@@ -36,3 +41,9 @@ def traf_names():
 
 def Rexpr_names():
     return ['IL2Ra', 'IL2Rb', 'gc', 'IL15Ra', 'IL7Ra', 'IL9R', 'IL4Ra', 'IL21Ra']
+
+def plot_conf_int(ax, x_axis, y_axis, color, label):
+    """ Calculates the 95% confidence interval for y-axis data and then plots said interval. The percentiles are found along axis=1. """
+    y_axis_top = np.percentile(y_axis, 97.5, axis=1)
+    y_axis_bot = np.percentile(y_axis, 2.5, axis=1)
+    ax.fill_between(x_axis, y_axis_top, y_axis_bot, color=color, alpha=0.5, label=label)
