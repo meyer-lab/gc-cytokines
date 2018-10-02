b4536c72d823e342f7b8774c7c4cfd844a5b090a
adamcweiner <30814024+adamcweiner@users.noreply.github.com>
2018-10-01 17:17:28 -0700

    Add threadpool to overcome slow pre-treatment handling. (#266)

--- run_fit_others.py
+++ run_fit_others.py
@@ -5,7 +5,7 @@ from ckine.fit_others import build_model
 import pymc3 as pm
 
 if __name__ == "__main__": #only go into this loop if you're running fit.py directly instead of running a file that calls fit.py
-    M = build_model(pretreat=False)
+    M = build_model(pretreat=True)
     M.build()
     M.sampling()
     pm.backends.text.dump("IL4-7_model_results", M.trace) #instead of pickling data we dump it into file that can be accessed by read_fit_data.py
