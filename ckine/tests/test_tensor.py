da2ea9bacd26e799fa4026e713c9dd3cf0531cda
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2018-12-14 14:03:07 -0800

    Reduce the size of the tensor used in the R2X test

--- ckine/tests/test_tensor.py
+++ ckine/tests/test_tensor.py
@@ -12,17 +12,17 @@ class TestModel(unittest.TestCase):
     '''Test Class for Tensor related work.'''
     def test_R2X(self):
         '''Test to ensure R2X for higher components is larger.'''
-        tensor = np.random.rand(20,35,100,20)
+        tensor = np.random.rand(12, 10, 15, 14)
         arr = []
-        for i in range(1,8):
+        for i in range(1, 8):
             factors = perform_decomposition(tensor, i)
             R2X = find_R2X(tensor, factors)
             arr.append(R2X)
         for j in range(len(arr)-1):
             self.assertTrue(arr[j] < arr[j+1])
         #confirm R2X is >= 0 and <=1
-        self.assertGreaterEqual(np.min(arr),0)
-        self.assertLessEqual(np.max(arr),1)
+        self.assertGreaterEqual(np.min(arr), 0)
+        self.assertLessEqual(np.max(arr), 1)
 
     def test_tensor_parameters(self, r=1):
         '''Function to ensure if rate parameters change in the model code then an error should warn us to update tensor generation code.'''
