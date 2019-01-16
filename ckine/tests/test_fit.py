ef9e068dec151a886fc44773161cdc1a5f2b9e3a
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2019-01-15 20:07:37 -0800

    Add tests that the fitting code at least builds (#321)
    * Add testing for fitting code.
    * Fixed?

---
+++ ckine/tests/test_fit.py
@@ -0,0 +1,18 @@
+"""
+Test that the fitting code can at least build the likelihood model.
+"""
+import unittest
+
+
+class TestFit(unittest.TestCase):
+    def test_fitIL2_15(self):
+        """ Test that the IL2/15 model can build. """
+        from ..fit import build_model
+        M = build_model()
+        M.build()
+
+    def test_fitIL4_7(self):
+        """ Test that the IL4/7 model can build. """
+        from ..fit_others import build_model
+        M = build_model()
+        M.build()
