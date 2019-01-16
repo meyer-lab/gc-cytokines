a64ae1e7c83f38a6ecce6525819e1cf90b2e7ccd
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2019-01-16 15:25:17 -0800

    Reworked code to put prestim into normal function. (#318)
    * Reworked code to put prestim into normal function.
    * This now enables adjoint sensitivity on prestimulation fitting.

--- ckine/differencing_op.py
+++ ckine/differencing_op.py
@@ -1,74 +1,9 @@
 """
 Theano Op for using differencing for Jacobian calculation.
 """
-import concurrent.futures
 import numpy as np
 from theano.tensor import dot, dmatrix, dvector, Op
-from .model import runCkineU, nSpecies, nParams, runCkineUP, runCkinePreT, runCkineS, runCkineSP
-
-pool = concurrent.futures.ThreadPoolExecutor()
-
-
-class runCkinePreSOp(Op):
-    """ Op for pretreatment experiments. """
-    itypes, otypes = [dvector], [dvector]
-
-    def __init__(self, tpre, ts, postlig):
-        self.dOp = runCkinePreSOpDiff(tpre, ts, postlig)
-
-    def infer_shape(self, node, i0_shapes):
-        """ inferring shape """
-        assert len(i0_shapes) == 1
-        return [(nSpecies(), )]
-
-    def perform(self, node, inputs, outputs, params=None):
-        outputs[0][0] = self.dOp.runCkine(inputs[0])
-
-    def grad(self, inputs, g):
-        """ Calculate the runCkineOp gradient. """
-        return [dot(g[0], self.dOp(inputs[0]))]
-
-
-class runCkinePreSOpDiff(Op):
-    """ Differencing op for pretreatment experiments. """
-    itypes, otypes = [dvector], [dmatrix]
-
-    def __init__(self, tpre, ts, postlig):
-        assert ts.size == 1
-        assert postlig.size == 6
-
-        self.ts = ts
-        self.tpre = tpre
-        self.postlig = postlig
-
-    def runCkine(self, inputs):
-        """ function for runCkine """
-        outt = runCkinePreT(self.tpre, self.ts, inputs, self.postlig)
-
-        assert outt[1] >= 0
-        assert outt[0].size == nSpecies()
-
-        return np.squeeze(outt[0])
-
-    def perform(self, node, inputs, outputs, params=None):
-        x0 = inputs[0]
-        f0 = self.runCkine(x0)
-        epsilon = 1.0E-6
-
-        jac = np.zeros((len(x0), len(f0)), dtype=np.float64)
-        dxl = list()
-
-        for i in range(len(x0)):
-            dx = x0.copy()
-            dx[i] = dx[i] + epsilon
-            dxl.append(dx)
-
-        dxlr = pool.map(self.runCkine, dxl)
-
-        for i, item in enumerate(dxlr):
-            jac[i] = (item - f0)/epsilon
-
-        outputs[0][0] = np.transpose(jac)
+from .model import runCkineU, nSpecies, nParams, runCkineUP, runCkineS, runCkineSP
 
 
 class runCkineKineticOp(Op):
@@ -118,8 +53,8 @@ class runCkineDoseOp(Op):
     """ Runs model for a dose response at a single time point. """
     itypes, otypes = [dvector], [dvector]
 
-    def __init__(self, tt, condense, conditions):
-        self.dOp = runCkineOpDoseDiff(tt, condense, conditions)
+    def __init__(self, tt, condense, conditions, preT=0.0, prestim=None):
+        self.dOp = runCkineOpDoseDiff(tt, condense, conditions, preT, prestim)
 
     def infer_shape(self, node, i0_shapes):
         """ infering shape """
@@ -138,8 +73,12 @@ class runCkineOpDoseDiff(Op):
     """ Gradient of model for a dose response at a single time point. """
     itypes, otypes = [dvector], [dmatrix]
 
-    def __init__(self, tt, condense, conditions):
-        self.ts, self.condense, self.conditions = tt, condense, conditions
+    def __init__(self, tt, condense, conditions, preT, prestim):
+        self.ts, self.condense, self.conditions, self.preT, self.prestim = tt, condense, conditions, preT, prestim
+
+        if preT > 0.0:
+            assert prestim.size == 6
+
         assert tt.size == 1
         assert condense.size == nSpecies() # Check that we're condensing a species vector
         assert conditions.shape[1] == 6 # Check that this is a matrix of ligands
@@ -151,11 +90,11 @@ class runCkineOpDoseDiff(Op):
         rxntfr = np.concatenate((self.conditions, rxntfr), axis=1)
 
         if sensi is False:
-            outt = runCkineUP(self.ts, rxntfr)
+            outt = runCkineUP(self.ts, rxntfr, self.preT, self.prestim)
             assert outt[1] >= 0
             return np.dot(outt[0], self.condense)
 
-        outt = runCkineSP(self.ts, rxntfr, self.condense)
+        outt = runCkineSP(self.ts, rxntfr, self.condense, self.preT, self.prestim)
         assert outt[0].shape == (self.conditions.shape[0], )
         assert outt[1] >= 0
 
--- ckine/fit_others.py
+++ ckine/fit_others.py
@@ -4,8 +4,8 @@ This file includes the classes and functions necessary to fit the IL4 and IL7 mo
 from os.path import join
 import pymc3 as pm, theano.tensor as T, os
 import numpy as np, pandas as pds
-from .model import getTotalActiveSpecies, getTotalActiveCytokine
-from .differencing_op import runCkineDoseOp, runCkinePreSOp
+from .model import getTotalActiveSpecies, getTotalActiveCytokine, nSpecies, getActiveSpecies, internalStrength, halfL, getCytokineSpecies
+from .differencing_op import runCkineDoseOp
 
 class IL4_7_activity:
     """ This class is responsible for calculating residuals between model predictions and the data from Gonnord figure S3B/C """
@@ -67,19 +67,30 @@ class crosstalk:
 
     def singleCalc(self, unkVec, pre_cytokine, pre_conc, stim_cytokine, stim_conc):
         """ This function generates the active vector for a given unkVec, cytokine used for inhibition and concentration of pretreatment cytokine. """
-        unkVec2 = T.set_subtensor(unkVec[pre_cytokine], pre_conc)
+        unkVec2 = T.set_subtensor(unkVec[stim_cytokine], stim_conc)
 
         # create ligands array for stimulation at t=0
-        ligands = np.zeros((6))
-        ligands[stim_cytokine] = stim_conc
-        ligands[pre_cytokine] = pre_conc    # assume pretreatment ligand stays constant
+        ligands = np.zeros((1, 6))
+        ligands[0, stim_cytokine] = stim_conc
+        ligands[0, pre_cytokine] = pre_conc    # assume pretreatment ligand stays constant
 
-        Op = runCkinePreSOp(tpre=self.ts, ts=self.ts, postlig=ligands)
+        prelig = np.zeros(6)
+        prelig[pre_cytokine] = pre_conc
+
+        condvec = np.zeros(nSpecies())
+        condvec[np.where(getActiveSpecies())] = 1.0
+        condvec[np.array(np.where(getActiveSpecies())) + halfL()] = internalStrength()
+        condvecCyto = np.zeros(nSpecies())
+        condvecCyto[getCytokineSpecies()[stim_cytokine]] = condvec[getCytokineSpecies()[stim_cytokine]]
+        condvecCyto[getCytokineSpecies()[stim_cytokine] + halfL()] = condvec[getCytokineSpecies()[stim_cytokine] + halfL()]
+        # TODO: Check this
+
+        Op = runCkineDoseOp(preT=self.ts, tt=self.ts, conditions=ligands, condense=condvecCyto, prestim=prelig)
 
         # perform the experiment
         outt = Op(unkVec2)
 
-        return getTotalActiveCytokine(stim_cytokine, outt) # only look at active species associated with stimulation cytokine
+        return outt[0] # only look at active species associated with stimulation cytokine
 
     def singleCalc_no_pre(self, unkVec):
         ''' This function generates the active vector for a given unkVec, cytokine, and concentration. '''
@@ -102,9 +113,6 @@ class crosstalk:
         IL4stim_no_pre = IL4stim_no_pre  / (IL4stim_no_pre + scales[0])
         IL7stim_no_pre = IL7stim_no_pre  / (IL7stim_no_pre + scales[1])
 
-        # add 6 zeros to front of unkVec to handle ligands within unkVec
-        unkVec = T.concatenate((T.zeros(6, dtype=np.float64), unkVec))
-
         # IL7 pretreatment with IL4 stimulation
         actVec_IL4stim = T.stack((list(self.singleCalc(unkVec, 2, x, 4, self.cytokM[0, 4]) for x in self.pre_IL7)))
 
@@ -118,7 +126,7 @@ class crosstalk:
 
         case1 = (1-(actVec_IL4stim/IL4stim_no_pre))    # % inhibition of IL4 act. after IL7 pre.
         case2 = (1-(actVec_IL7stim/IL7stim_no_pre))    # % inhibition of IL7 act. after IL4 pre.
-        inh_vec = T.concatenate((case1, case1, case1, case2, case2, case2 ))   # mimic order of CSV file
+        inh_vec = T.concatenate((case1, case1, case1, case2, case2, case2))   # mimic order of CSV file
 
         return inh_vec - self.fit_data
 
