9f7d2224643a3d51e8cd7d1ac1e405cb088d1f7e
alifarhat30 <32119344+alifarhat30@users.noreply.github.com>
2019-02-22 07:34:15 -0800

    Fitting model with no trafficking (#357)
    * Update files to fit without traficking
    
    * Fixing
    
    * ignore trafficking rates in fullModel
    
    * revert changes
    
    * force activeEndo to 0 as well
    
    * remove parameters
    
    * Linting
    
    * Revert
    
    * Update fit.py
    
    * Update fit.py

--- run_fit.py
+++ run_fit.py
@@ -8,12 +8,12 @@ if __name__ == "__main__": #only go into this loop if you're running fit.py dire
     if sys.argv[1] == '1':
         print('Running IL2/15 model')
         from ckine.fit import build_model
-        filename = "IL2_model_results"
+        filename = "IL2_15_no_traf"
     else:
         print('Running IL4/7 model')
         from ckine.fit_others import build_model
         filename = "IL4-7_model_results"
 
-    M = build_model()
+    M = build_model(traf = False)
     trace = sampling(M.M)
     pm.backends.text.dump(filename, trace) #instead of pickling data we dump it into file that can be accessed by read_fit_data.py
