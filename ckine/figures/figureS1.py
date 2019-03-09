beec8283eb2ce33edac95f4b854966b7193f3a4a
adamcweiner <30814024+adamcweiner@users.noreply.github.com>
2019-03-09 10:44:01 -0800

    Fig 1, 2 & S1 text (#374)
    * add text from drive
    
    * shuffle panels of figS1 around
    
    * add FigS1 caption

--- ckine/figures/figureS1.py
+++ ckine/figures/figureS1.py
@@ -8,7 +8,7 @@ from .figure1 import pstat_act, violinPlots, rateComp
 def makeFigure():
     """Get a list of the axis objects and create a figure"""
     # Get list of axis objects
-    ax, f = getSetup((8, 6), (2, 3), mults=[0], multz={0:2}, empts=[5])
+    ax, f = getSetup((8, 6), (2, 3), mults=[1], multz={1:2}, empts=[3])
 
     # Add subplot labels
     for ii, item in enumerate(ax):
@@ -17,8 +17,8 @@ def makeFigure():
     unkVec, scales = import_samples_2_15(Fig1=False)
     kfwd_avg, kfwd_std = kfwd_info(unkVec)
     print("kfwd = " + str(kfwd_avg) + " +/- " + str(kfwd_std))
-    rateComp(ax[0], unkVec)
-    pstat_act(ax[1], unkVec, scales, Fig1=False)
+    pstat_act(ax[0], unkVec, scales, Fig1=False)
+    rateComp(ax[1], unkVec)
     violinPlots(ax[2:4], unkVec, scales, Fig1=False)
 
 
