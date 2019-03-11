19811b1a3d77fdb9f1e2e671c0599cac66927cfd
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2019-03-11 16:18:49 -0700

    A little more tensor cleaning (#382)
    * Update Tensor_analysis.py
    
    * Update figure3.py
    
    * Update figureS2.py
    
    * Update test_tensor.py
    
    * Update Tensor_analysis.py
    
    * Update figure3.py
    
    * Update Tensor_analysis.py
    
    * Update Tensor_analysis.py
    
    * Update Tensor_analysis.py
    
    * Update test_tensor.py
    
    * Update test_tensor.py
    
    * Update figure3.py
    
    * Update figureS2.py
    
    * Update figureS2.py
    
    * Update Tensor_analysis.py

--- ckine/Tensor_analysis.py
+++ ckine/Tensor_analysis.py
@@ -3,74 +3,44 @@ Analyze tensor from tensor_generation and plotting.
 """
 import numpy as np
 import tensorly as tl
-from tensorly.decomposition import parafac
-from tensorly.decomposition import tucker
-from tensorly.metrics.regression import variance as tl_var, standard_deviation
+from tensorly.decomposition import non_negative_parafac, non_negative_tucker
+from tensorly.metrics.regression import variance as tl_var
 
-def tensorly_backend(bknd):
-    '''Function to convert back and forth between numpy and cupy backends. Always works with numpy unless set as False which switches to cupy.'''
-    if bknd == 0:
-        tl.set_backend('numpy')
-    elif bknd == 1:
-        tl.set_backend('cupy')
+backend = 'numpy' #Only place to choose what the backend should be. numpy = 0. cupy = 1. other backends we desire = 2, ... 
+tl.set_backend(backend) #Set the backend within every file that imports from Tensor_analysis.py
 
-backend = 0 #Only place to choose what the backend should be. numpy = 0. cupy = 1. other backends we desire = 2, ... 
-tensorly_backend(bknd = backend) #Set the backend within every file that imports from Tensor_analysis.py
+# Set whether or not we subtract in one place so we're consistent
+subtract = False
 
-def z_score_values(A, subtract):
+def z_score_values(A):
     ''' Function that takes in the values tensor and z-scores it. '''
-    sigma = np.std(A, axis=(0, 1))
-    mu = tl.mean(A, axis=(0, 1))
+    sigma = np.std(A, axis=(0, 2))
+    mu = tl.mean(A, axis=(0, 2))
     if subtract is False:
         mu[:] = 0.0
-    return (A - mu[None, None, :]) / sigma[None, None, :]
+    return (A - mu[None, :, None]) / sigma[None, :, None]
 
 
 def R2X(reconstructed, original):
     ''' Calculates R2X of two tensors. '''
     return 1.0 - tl_var(reconstructed - original) / tl_var(original)
 
-def perform_decomposition(tensor, r, subt):
-    '''Apply z scoring and perform PARAFAC decomposition'''
-    values_z = z_score_values(tensor, subtract = subt)
-    factors = parafac(values_z, rank = r) # can do verbose and tolerance (tol)
-    return factors
+def perform_decomposition(tensor, r):
+    ''' Apply z scoring and perform PARAFAC decomposition. '''
+    return non_negative_parafac(z_score_values(tensor), r, tol=1.0E-9, n_iter_max=1000)
 
-def perform_tucker(tensor, rank_list, subt):
+def perform_tucker(tensor, rank_list):
     '''Function to peform tucker decomposition.'''
-    values_z = z_score_values(tensor, subtract=subt)
-    out = tucker(values_z, ranks=rank_list) # index 0 is for core tensor, index 1 is for factors; out is a list of core and factors
+    out = non_negative_tucker(z_score_values(tensor), rank_list, tol=1.0E-9, n_iter_max=1000) # index 0 is for core tensor, index 1 is for factors; out is a list of core and factors
     return out
 
-def find_R2X_tucker(values, out, subt):
-    '''Compute R2X for the tucker decomposition.'''
-    z_values = z_score_values(values, subtract = subt)
-    values_reconstructed = tl.tucker_to_tensor(out[0], out[1])        
-    return R2X(values_reconstructed, z_values)
+def find_R2X_tucker(values, out):
+    '''Compute R2X for the tucker decomposition.'''     
+    return R2X(tl.tucker_to_tensor(out[0], out[1]) , z_score_values(values))
 
-def reorient_one(factors, component_index):
-    """Function that takes in the 4 factor matrices and decides if that column index should flip or not and then flips it."""
-    factors_idx = [factors[0][:,component_index], factors[1][:,component_index], factors[2][:,component_index]]
-    component_means = tl.tensor([tl.mean(factors_idx[0]**3), tl.mean(factors_idx[1]**3), tl.mean(factors_idx[2]**3)])
-    if tl.sum(component_means < 0) >= 2 and tl.sum(component_means < 0) <= 3: #if at least 2 are negative, then flip the negative component and keep others unchanged
-        count = 1
-        for index, factor_idx in enumerate(factors_idx):
-            if component_means[index] < 0 and count < 3:
-                factors[index][:, component_index] = factor_idx * -1
-                count += 1
-    return factors
-
-def reorient_factors(factors):
-    """This function is to reorient the factors if at least one component in two factors matrices are negative."""
-    for jj in range(factors[0].shape[1]):
-        factors = reorient_one(factors, jj)
-    return factors
-
-def find_R2X(values, factors, subt):
+def find_R2X(values, factors):
     '''Compute R2X. Note that the inputs values and factors are in numpy.'''
-    z_values = z_score_values(values, subtract = subt)
-    values_reconstructed = tl.kruskal_to_tensor(factors)
-    return R2X(values_reconstructed, z_values)
+    return R2X(tl.kruskal_to_tensor(factors), z_score_values(values))
 
 def scale_time_factors(factors, component_index):
     """Scale the timepoint factor component by dividing the mean and then in the values plot multiply the values by that same number."""
--- ckine/tests/test_tensor.py
+++ ckine/tests/test_tensor.py
@@ -4,7 +4,7 @@ Unit test file.
 import unittest
 import numpy as np
 import tensorly as tl
-from ..Tensor_analysis import find_R2X, perform_decomposition, reorient_factors, scale_all
+from ..Tensor_analysis import find_R2X, perform_decomposition, scale_all
 from ..tensor_generation import findy
 
 class TestModel(unittest.TestCase):
@@ -14,8 +14,8 @@ class TestModel(unittest.TestCase):
         tensor = tl.tensor(np.random.rand(12, 10, 15))
         arr = []
         for i in range(1, 8):
-            factors = perform_decomposition(tensor, i, subt = True)
-            R2X = find_R2X(tensor, factors, subt = True)
+            factors = perform_decomposition(tensor, i)
+            R2X = find_R2X(tensor, factors)
             arr.append(R2X)
         for j in range(len(arr)-1):
             self.assertTrue(arr[j] < arr[j+1])
@@ -23,24 +23,10 @@ class TestModel(unittest.TestCase):
         self.assertGreaterEqual(tl.min(arr), 0)
         self.assertLessEqual(tl.max(arr), 1)
 
-    def test_tensor_parameters(self, r=1):
-        '''Function to ensure if rate parameters change in the model code then an error should warn us to update tensor generation code.'''
-        y_combos, new_mat = findy(r,50)[0:2]
-        self.assertTrue(y_combos.shape[0] == new_mat.shape[0])
-
-    def test_reorientation(self, n_comp = 20):
-        """Test if reorienting the factors matrices changes anything about the original tensor itself."""
-        tensor = tl.tensor(np.random.rand(20, 35, n_comp))
-        factors = perform_decomposition(tensor, n_comp-1, subt = True)
-        reconstruct_old = tl.kruskal_to_tensor(factors)
-        new_factors = reorient_factors(factors)
-        reconstruct_new = tl.kruskal_to_tensor(new_factors)
-        np.testing.assert_almost_equal(tl.to_numpy(reconstruct_old), tl.to_numpy(reconstruct_new))
-
     def test_rescale_all(self, n_comp = 20):
         """Test if rescaling every component keeps the tensor the same."""
         tensor = tl.tensor(np.random.rand(20, 35, n_comp))
-        factors = perform_decomposition(tensor, n_comp-1, subt = True)
+        factors = perform_decomposition(tensor, n_comp-1)
         reconstruct_old = tl.kruskal_to_tensor(factors)
         newfactors = scale_all(factors)
         reconstruct_new = tl.kruskal_to_tensor(newfactors)
