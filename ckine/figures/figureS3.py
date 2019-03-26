501f602d530837808dde42e4216b9d8f2c7971e3
alifarhat30 <32119344+alifarhat30@users.noreply.github.com>
2019-03-26 11:57:47 -0400

    improve fig3 (#392)
    * improve fig3
    
    * more fixing
    
    * Update figure3.py
    
    * More updates
    
    * linting

--- ckine/figures/figureS3.py
+++ ckine/figures/figureS3.py
@@ -6,14 +6,13 @@ import numpy as np
 import tensorly as tl
 from .figureCommon import subplotLabel, getSetup, plot_cells, plot_ligands, plot_timepoints, values
 from ..Tensor_analysis import perform_tucker, find_R2X_tucker
-from ..tensor_generation import data
+from ..tensor_generation import data, cell_names
 
 def makeFigure():
     """Get a list of the axis objects and create a figure"""
     x, y = 2, 3
-    ax, f = getSetup((7.5, 5), (x, y))
-
-    cell_names = ['Naive Th', 'Mem Th', 'Naive Treg', 'Mem Treg','Naive CD8+', 'Mem CD8+', 'NK', 'NKT']
+    ax, f = getSetup((9, 5), (x, y))
+    ax[3].axis('off')
 
     rank_list = [3, 2*x, 2*x]
     out = perform_tucker(values, rank_list)
@@ -28,7 +27,7 @@ def makeFigure():
 
         plot_cells(ax[row*y + 1], tl.to_numpy(factors[1]), compNum, compNum + 1, cell_names, ax_pos = row*y + 1)
         if compNum < rank_list[2]:
-            plot_ligands(ax[row*y + 2], tl.to_numpy(factors[2]), compNum, compNum + 1, ax_pos = row*y + 2)
+            plot_ligands(ax[row*y + 2], tl.to_numpy(factors[2]), compNum, compNum + 1, ax_pos = row*y + 2, fig3 = False)
 
         # Set axes to center on the origin, and add labels
         for col in range(1, y):
@@ -41,9 +40,7 @@ def makeFigure():
             ax[row*y + col].set_xlim(-0.05, x_max)
             ax[row*y + col].set_ylim(-0.05, y_max)
 
-    subplotLabel(ax[3], string.ascii_uppercase[2])
-    ax[3].set_title('Core Tensor')
-        
+    subplotLabel(ax[3], string.ascii_uppercase[2])        
     f.tight_layout()
 
     return f
