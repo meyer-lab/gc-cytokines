da3f5e5b2e3afbe988ae34254442edfa499efaf2
alifarhat30 <32119344+alifarhat30@users.noreply.github.com>
2019-05-23 12:42:10 -0700

    Add IL7 and 7Ra to factorization Plots (#467)
    * add 7ra to factorization
    
    * linting
    
    * fix component numbers
    
    * 3 components
    
    * better view
    
    * weird failure
    
    * updated code

--- ckine/make_tensor.py
+++ ckine/make_tensor.py
@@ -10,7 +10,7 @@ from .imports import import_Rexpr, import_samples_2_15, import_pstat
 rxntfR, _ = import_samples_2_15(N=1, tensor=True)
 rxntfR = np.squeeze(rxntfR)
 
-n_lig = 3  # Set the number of different cytokines used to make the tensor to 3
+n_lig = 4  # Set the number of different cytokines used to make the tensor to 4
 
 
 def ySolver(matIn, ts, tensor=True):
@@ -54,16 +54,17 @@ def meshprep():
     ILs = np.flip(ILs)
 
     '''Goal is to make one cell expression levels by len(mat) for every cell
-    Make mesh grid of all ligand concentrations, First is IL-2 WT, Second is IL-2 Mutant; Third is IL-15.
+    Make mesh grid of all ligand concentrations, First is IL-2 WT, Second is IL-2 Mutant; Third is IL-15; Fourth is IL7
     Set interleukins other than IL2&15 to zero. Should be of shape 3(IL2,mutIL2,IL15)*(len(ILs)) by 6 (6 for all ILs)'''
     concMesh = np.vstack((np.array(np.meshgrid(ILs, 0, 0, 0, 0, 0)).T.reshape(-1, 6),
                           np.array(np.meshgrid(ILs, 0, 0, 0, 0, 0)).T.reshape(-1, 6),
-                          np.array(np.meshgrid(0, ILs, 0, 0, 0, 0)).T.reshape(-1, 6)))
+                          np.array(np.meshgrid(0, ILs, 0, 0, 0, 0)).T.reshape(-1, 6),
+                          np.array(np.meshgrid(0, 0, ILs, 0, 0, 0)).T.reshape(-1, 6)))
     '''Repeat the cytokine stimulations (concMesh) an X amount of times where X here is number of cells (12).
     Just stacks up concMesh on top of each other 12 times (or however many cells are available)'''
     concMesh_stacked = np.tile(concMesh, (len(cell_names), 1))
 
-    # Set receptor levels for IL7Ra, IL9R, IL4Ra, IL21Ra to one. We won't use them for IL2-15 model. Second argument can also be set to 4 since we only have IL2Ra, IL2Rb, gc, IL15Ra measured.
+    # Set receptor levels for IL9R, IL4Ra, IL21Ra to 0. We won't use them for IL2-15 model. Second argument can also be set to 4 since we only have IL2Ra, IL2Rb, gc, IL15Ra, IL7Ra measured.
     no_expression = np.ones((numpy_data.shape[0], 8 - numpy_data.shape[1])) * 0.0
     # Need to convert numbers to expression values
     numpy_data[:, :] = (numpy_data[:, :] * rxntfR[17]) / (1. + ((rxntfR[20] * (1. - rxntfR[19])) / (rxntfR[21] * rxntfR[19])))  # constant according to measured number per cell
@@ -72,7 +73,7 @@ def meshprep():
 
     '''Concatenate to obtain the new meshgrid.
     Conc_recept_cell basically repeats the initial stimulation concentration for all ligands of interest an X amount of times (X = # of cells)
-    The first [len(ILs)*3] are for cell 1, second [len(ILs)*3] are for cell 2, etc...'''
+    The first [len(ILs)*4] are for cell 1, second [len(ILs)*4] are for cell 2, etc...'''
     Conc_recept_cell = np.concatenate((concMesh_stacked, receptor_repeats), axis=1)
     return Conc_recept_cell, concMesh, concMesh_stacked, cell_names
 
@@ -80,7 +81,7 @@ def meshprep():
 def prep_tensor(numlig, n_timepoints):
     """Function to solve the model for initial conditions in meshprep()."""
     Conc_recept_cell, concMesh, concMesh_stacked, cell_names = meshprep()
-    idx_ref = concMesh.shape[0] / numlig  # Provides a reference for the order of idices at which the mutant is present.
+    idx_ref = int(concMesh.shape[0] / numlig)  # Provides a reference for the order of indices at which the mutant is present.
 
     # generate n_timepoints evenly spaced timepoints to 4 hrs
     ts = np.logspace(-3., np.log10(4 * 60.), n_timepoints)
@@ -89,13 +90,21 @@ def prep_tensor(numlig, n_timepoints):
     # Allocate a y_of_combos
     y_of_combos = np.zeros((len(Conc_recept_cell), ts.size, nSpecies()))
 
-    for jj in range(Conc_recept_cell.shape[0]):
-        if jj % concMesh.shape[0] < idx_ref or jj % concMesh.shape[0] >= idx_ref * (numlig - 1):
-            # Solve using the wildtype model.These indices are for WT IL-2 and WT IL-15.
-            y_of_combos[jj] = ySolver(Conc_recept_cell[jj, :], ts)
+    #Find the indices where IL-2 mutant lies in the meshgrid of all tensor conditions.
+    mut2 = np.arange(0, Conc_recept_cell.shape[0], idx_ref)
+    rmvs = mut2[np.arange(1, mut2.size, numlig)]
+
+    mutIL2_idxs = np.zeros((rmvs.size, idx_ref))
+    for jj in range(len(rmvs)):
+        mutIL2_idxs[jj] = np.array(range(rmvs[jj], rmvs[jj]+idx_ref)) #Find the indices where the IL2-mutant is.
+
+    for jj, row in enumerate(Conc_recept_cell):
+        if jj in mutIL2_idxs:
+            #Solve using the mutant IL2 solver for these particular indices.
+            y_of_combos[jj] = ySolver_IL2_mut(row, ts)
         else:
-            # Solve using the mutant model for mut-IL2
-            y_of_combos[jj] = ySolver_IL2_mut(Conc_recept_cell[jj, :], ts)
+            #Solve using the WT solver for each of IL2, IL15, and IL7.
+            y_of_combos[jj] = ySolver(row, ts)
     return y_of_combos, Conc_recept_cell, concMesh, concMesh_stacked, cell_names
 
 
