15b4557de9eeaa36394f27907a76cb71f509808b
alifarhat30 <32119344+alifarhat30@users.noreply.github.com>
2019-07-23 18:50:54 -0500

    Issue #466 (#535)
    * Issue #466
    
    * linting
    
    * linting
    
    * fig5 edits
    
    * subplot labels

--- ckine/figures/figure5.py
+++ ckine/figures/figure5.py
@@ -3,22 +3,25 @@ This creates Figure 5. CP decomposition of measured pSTAT data.
 """
 import string
 import numpy as np
-import matplotlib.cm as cm
+from scipy.stats import pearsonr
 from .figureCommon import subplotLabel, getSetup, plot_cells, plot_ligands, plot_timepoints
-from .figure3 import plot_R2X
+from .figure3 import plot_R2X, factors_activity as predicted_factors
 from ..tensor import perform_decomposition, z_score_values
 from ..imports import import_pstat
 
 cell_dim = 0  # For this figure, the cell dimension is along the first [python index 0].
 
-
 def makeFigure():
     """Get a list of the axis objects and create a figure"""
     # Get list of axis objects
-    ax, f = getSetup((7.5, 2.5), (1, 4))
+    ax, f = getSetup((7.5, 5), (2, 4))
+    ax[5].axis('off')
+    ax[6].axis('off')
+    ax[7].axis('off')
 
+    # Add subplot labels
     for ii, item in enumerate(ax):
-        subplotLabel(item, string.ascii_uppercase[ii])  # Add subplot labels
+        subplotLabel(item, string.ascii_uppercase[ii])
 
     _, cell_names, IL2_data, IL15_data, _, _ = import_pstat()
 
@@ -30,30 +33,33 @@ def makeFigure():
     measured_tensor = np.reshape(concat, (len(cell_names), 5, IL2.shape[1] * 2))
     measured_tensor = z_score_values(measured_tensor, cell_dim)
 
-    factors_activity = []
+    experimental_factors = []
     for jj in range(5):
         factors = perform_decomposition(measured_tensor, jj + 1)
-        factors_activity.append(factors)
+        experimental_factors.append(factors)
 
-    plot_R2X(ax[0], measured_tensor, factors_activity)
+    plot_R2X(ax[0], measured_tensor, experimental_factors)
 
     n_comps = 2
-    factors_activ = factors_activity[n_comps - 1]  # First dimension is cells. Second is time. Third is ligand.
-    plot_timepoints(ax[1], np.array([0.0, 0.5, 1., 2., 4.]) * 60., factors_activ[1])  # Time is the second dimension in this case because reshaping only correctly did 11*4*24
-    plot_cells(ax[2], factors_activ[0], 1, 2, cell_names)
-    plot_ligands(ax[3], factors_activ[2], ligand_names=['IL-2', 'IL-15'])
+    experimental_decomposition = experimental_factors[n_comps - 1]  # First dimension is cells. Second is time. Third is ligand.
+    plot_timepoints(ax[1], np.array([0.0, 0.5, 1., 2., 4.]) * 60., experimental_decomposition[1])  # Time is the second dimension in this case because reshaping only correctly did 11*4*24
+    plot_cells(ax[2], experimental_decomposition[0], 1, 2, cell_names)
+    plot_ligands(ax[3], experimental_decomposition[2], ligand_names=['IL-2', 'IL-15'])
 
+    #Predicted tensor
+    predicted_cell_factors = predicted_factors[n_comps - 1]
+    correlation_cells(ax[4], experimental_decomposition[0], predicted_cell_factors[1])
     return f
 
-
-def correlation_cells(ax, experimental, predicted, cell_names):
-    """Function that takes in predicted and experimental components from cell decomposion and plots them against each other."""
-    colors = cm.rainbow(np.linspace(0, 1, len(cell_names)))
-    markersCells = ['^', '*', 'D', 's', 'X', 'o', '4', 'H', 'P', '*', 'D', 's', 'X']
-    for ii, cell_name in enumerate(cell_names):
-        ax.scatter(experimental, predicted, c=[colors[ii]], marker=markersCells[ii], label=cell_name, alpha=0.75)
-
-
-def correlation_ligands(ax, experimental, predicted):
-    """Function that takes in predicted and experimental components from ligand decomposion and plots them against each other."""
-    ax.scatter(experimental, predicted)
+def correlation_cells(ax, experimental, predicted):
+    """Function that takes in predicted and experimental components from cell decomposion and gives a bar graph of the Pearson Correlation Coefficients."""
+    coefficients = []
+    idx = []
+    for ii in range(experimental.shape[1]):
+        for jj in range(predicted.shape[1]):
+            idx.append(str((ii+1, jj+1)))
+            coefficients.append(pearsonr(experimental[:, ii], predicted[:, jj])[0])
+    ax.bar(np.arange(len(coefficients)), np.array(coefficients), align="center")
+    ax.set_xticklabels(idx)
+    ax.set_xlabel("Component Number (Experimental, Predicted)")
+    ax.set_ylabel("Pearson Correlation Coefficient")
