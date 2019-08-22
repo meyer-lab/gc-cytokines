468e9b2754bb6c5f1f2861bdae38a7d01ab655fb
zoekim <zoeekimm@gmail.com>
2019-08-22 11:05:34 -0700

    Add docstring

--- ckine/figures/figure4.py
+++ ckine/figures/figure4.py
@@ -74,7 +74,7 @@ def makeFigure():
 
     catplot_comparison(ax[1], df)  # compare experiments to model predictions
     plot_corrcoef(ax[2], tps)  # find correlation coefficients
-    global_legend(ax[2])  # add legend subplots A-C
+    global_legend(ax[2]) # add legend subplots A-C
 
     plot_exp_v_pred(ax[3:9], cell_subset=["NK", "CD8+", "T-reg"])  # NK, CD8+, and Treg subplots taken from fig S5
 
@@ -132,6 +132,7 @@ def plot_corrcoef(ax, tps):
 
 
 def global_legend(ax):
+    """ Create legend for colors and markers in subplots A-C. """
     purple = mpatches.Patch(color='darkorchid', label='IL-2')
     yellow = mpatches.Patch(color='goldenrod', label='IL-15')
     circle = mlines.Line2D([], [], color='black', marker='o', linestyle='None', markersize=6, label='Experimental')
