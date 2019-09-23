27077581805816d9fc68d5cd9d9eac1d90ef5294
Aaron Meyer <a@ameyer.me>
2019-09-23 11:47:11 -0700

    Bump.

--- ckine/figures/figureCommon.py
+++ ckine/figures/figureCommon.py
@@ -14,10 +14,10 @@ from ..imports import import_pstat
 
 matplotlib.rcParams['legend.labelspacing'] = 0.2
 matplotlib.rcParams['legend.fontsize'] = 8
-matplotlib.rcParams['xtick.major.pad'] = 1.5
-matplotlib.rcParams['ytick.major.pad'] = 1.5
-matplotlib.rcParams['xtick.minor.pad'] = 1.4
-matplotlib.rcParams['ytick.minor.pad'] = 1.4
+matplotlib.rcParams['xtick.major.pad'] = 1.0
+matplotlib.rcParams['ytick.major.pad'] = 1.0
+matplotlib.rcParams['xtick.minor.pad'] = 0.9
+matplotlib.rcParams['ytick.minor.pad'] = 0.9
 matplotlib.rcParams['legend.handletextpad'] = 0.5
 matplotlib.rcParams['legend.handlelength'] = 0.5
 matplotlib.rcParams['legend.framealpha'] = 0.5
