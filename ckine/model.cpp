1e2ba9d9e534d6555d6290d13064f2d1c738b195
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2019-10-06 20:33:45 -0700

    Update model.cpp

--- ckine/model.cpp
+++ ckine/model.cpp
@@ -32,7 +32,7 @@ using std::endl;
 using std::cout;
 using adept::adouble;
 
-constexpr double solveTol = 1.0E-5;
+constexpr double solveTol = 1.0E-6;
 
 static void errorHandler(int, const char *, const char *, char *, void *);
 int Jac(double, N_Vector, N_Vector, SUNMatrix, void *, N_Vector, N_Vector, N_Vector);
