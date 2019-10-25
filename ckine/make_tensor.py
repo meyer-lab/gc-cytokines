c501eb145f88770997f4d7715e3f91cbcc762ebf
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2019-10-25 16:49:25 -0700

    Remove Recursion changer

--- ckine/make_tensor.py
+++ ckine/make_tensor.py
@@ -52,12 +52,6 @@ def ySolver_IL2_mut(matIn, ts, mut):
 
 def meshprep(mut):
     """ Prepares the initial conditions for the tensor. The mutant condition here includes IL-2 mutein binding. """
-    a = sys.getrecursionlimit()
-    print(a)
-    print("Up Here!")
-    sys.setrecursionlimit(1000000)
-    a = sys.getrecursionlimit()
-    print(a)
     # Load the data from csv file
     _, numpy_data, cell_names = import_Rexpr()
     ILs, _, _, _ = import_pstat()  # Cytokine stimulation concentrations in nM
