1df4113d005d2645e8a193d322a9191be81f55fe
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2019-10-28 13:12:56 -0700

    Update test_op.py

--- ckine/tests/test_op.py
+++ ckine/tests/test_op.py
@@ -7,7 +7,7 @@ import theano.tensor as T
 from theano.tests import unittest_tools as utt
 import numpy as np
 from ..differencing_op import runCkineDoseOp
-from ..model import nSpecies, nParams, getTotalActiveSpecies, rxParams
+from ..model import nSpecies, nParams, getTotalActiveSpecies
 
 
 def setupJacobian(Op, unk):
