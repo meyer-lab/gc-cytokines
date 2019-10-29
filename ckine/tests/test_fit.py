2980e050d1863d8bdfcfff9b35a80781863ce02d
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2019-10-28 17:44:18 -0700

    Update test_fit.py

--- ckine/tests/test_fit.py
+++ ckine/tests/test_fit.py
@@ -2,6 +2,8 @@
 Test that the fitting code can at least build the likelihood model.
 """
 import unittest
+from ..fit import build_model as build_model2
+from ..fit_others import build_model as build_model4
 
 
 class TestFit(unittest.TestCase):
@@ -9,12 +11,10 @@ class TestFit(unittest.TestCase):
 
     def test_fitIL2_15(self):
         """ Test that the IL2/15 model can build. """
-        from ..fit import build_model
-        M = build_model()
+        M = build_model2()
         M.build()
 
     def test_fitIL4_7(self):
         """ Test that the IL4/7 model can build. """
-        from ..fit_others import build_model
-        M = build_model()
+        M = build_model4()
         M.build()
