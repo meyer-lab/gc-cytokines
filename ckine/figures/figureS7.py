7591466ee829c6621030f03c38614135f8b85df9
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2019-11-07 15:00:34 -0800

    Gradient Scalar Problem
    git status

--- ckine/figures/figureS7.py
+++ ckine/figures/figureS7.py
@@ -30,19 +30,19 @@ def Specificity(ax, cell_subset=None):
     """ Creates Theano Function for calculating Specificity gradient with respect to various parameters"""
 
     unkVec, _ = import_samples_2_15(N=1)
-    unkVec[0] = 1
     CondIL = np.zeros((1, 6), dtype=np.float64)
-    Op = runCkineDoseOp(tt=np.array(1.), condense=getTotalActiveSpecies().astype(np.float64), conditions=CondIL)
+    CondIL[0] = 1.
+    Op = runCkineDoseOp(tt=np.array(500.), condense=getTotalActiveSpecies().astype(np.float64), conditions=CondIL)
     unkVecTrunc = theano.tensor.zeros(24)
     unkVec = unkVec[6::].flatten()
     unkVecTrunc = T.set_subtensor(unkVecTrunc[0:], np.transpose(unkVec))
     a = Op(unkVecTrunc)
     print(a)
     
-    print(unkVecTrunc.eval())
+    
     unkVecT = unkVecTrunc#theano.tensor.zeros(30)
     _, receptor_data, cell_names_receptor = import_Rexpr()
-    S = OPgen(unkVecT, "T-reg", Op) / (OPgen(unkVecT, "T-reg", Op) + OPgen(unkVecT, "Naive Treg", Op) + OPgen(unkVecT, "Mem Treg", Op) + OPgen(unkVecT, "T-helper", Op) + OPgen(unkVecT, "Naive Th", Op) + OPgen(unkVecT, "Mem Th", Op) + OPgen(unkVecT, "NK", Op) + OPgen(unkVecT, "CD8+", Op) + OPgen(unkVecT, "Naive CD8+", Op) + OPgen(unkVecT, "Mem CD8+", Op))  #define function of specifivity
+    S = (OPgen(unkVecT, "T-reg", Op) / (OPgen(unkVecT, "T-reg", Op) + OPgen(unkVecT, "Naive Treg", Op) + OPgen(unkVecT, "Mem Treg", Op) + OPgen(unkVecT, "T-helper", Op) + OPgen(unkVecT, "Naive Th", Op) + OPgen(unkVecT, "Mem Th", Op) + OPgen(unkVecT, "NK", Op) + OPgen(unkVecT, "CD8+", Op) + OPgen(unkVecT, "Naive CD8+", Op) + OPgen(unkVecT, "Mem CD8+", Op)))
 
     Sgrad = T.grad(S, unkVecT)
     
@@ -58,24 +58,21 @@ def Specificity(ax, cell_subset=None):
 
 
 def OPgen(unkVec, CellTypes, Op):
-    #CondIL = np.zeros((1, 6), dtype=np.float64)
-    #CondIL[0] = 1
-    #Op = runCkineDoseOp(tt=np.array(500.), condense=getTotalActiveSpecies().astype(np.float64), conditions=CondIL)
     _, receptor_data, cell_names_receptor = import_Rexpr()
     cell_names_receptor = cell_names_receptor.tolist()
     OpHolder = np.zeros(len(CellTypes))
     CellTypes = [CellTypes]
+    predVec = theano.tensor.zeros(1)
 
     for i, Ctype in enumerate(CellTypes): #Update each vec for unique cell expression levels
         cell_data = receptor_data[cell_names_receptor.index(Ctype), :]
-        print(receptor_expression(cell_data[0], unkVec[11], unkVec[14], unkVec[13], unkVec[21]))
-        unkVec = T.set_subtensor(unkVec[16], receptor_expression(cell_data[0], unkVec[11], unkVec[14], unkVec[13], unkVec[21]))
-        unkVec = T.set_subtensor(unkVec[17], receptor_expression(cell_data[1], unkVec[11], unkVec[14], unkVec[13], unkVec[21]))
-        unkVec = T.set_subtensor(unkVec[18], receptor_expression(cell_data[2], unkVec[11], unkVec[14], unkVec[13], unkVec[21]))
+        unkVec = T.set_subtensor(unkVec[16], receptor_expression(cell_data[0], unkVec[11], unkVec[14], unkVec[13], unkVec[15]))
+        unkVec = T.set_subtensor(unkVec[17], receptor_expression(cell_data[1], unkVec[11], unkVec[14], unkVec[13], unkVec[15]))
+        unkVec = T.set_subtensor(unkVec[18], receptor_expression(cell_data[2], unkVec[11], unkVec[14], unkVec[13], unkVec[15]))
         unkVec = T.set_subtensor(unkVec[19], 0)
-        print(unkVec.eval())
+        #print(unkVec.eval())
         Cell_Op = Op(unkVec)
-        print(Cell_Op.eval())
-
+        predVec = T.set_subtensor(predVec[:], Cell_Op.eval().flatten())
+        print(predVec.eval())
 
     return Cell_Op
