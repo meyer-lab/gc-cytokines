80caea2118608c5b254230cdbc6dbcb1c89338aa
Aaron Meyer <ameyer@ucla.edu>
2019-11-07 20:56:56 -0800

    Fixed theano use.

--- ckine/figures/figureS7.py
+++ ckine/figures/figureS7.py
@@ -5,12 +5,12 @@ import string
 import numpy as np
 import matplotlib.cm as cm
 import theano.tensor as T
+import theano
 from scipy.optimize import least_squares
 from .figureCommon import subplotLabel, getSetup, plot_conf_int, plot_scaled_pstat
 from ..model import runCkineUP, getTotalActiveSpecies, receptor_expression, nParams
 from ..imports import import_Rexpr, import_pstat, import_samples_2_15
 from ..differencing_op import runCkineDoseOp
-from theano.tensor import *
 
 
 def makeFigure():
@@ -33,25 +33,24 @@ def Specificity(ax, cell_subset=None):
     CondIL = np.zeros((1, 6), dtype=np.float64)
     CondIL[0] = 1.
     Op = runCkineDoseOp(tt=np.array(500.), condense=getTotalActiveSpecies().astype(np.float64), conditions=CondIL)
-    unkVecTrunc = theano.tensor.zeros(24)
+    unkVecTrunc = T.zeros(24)
     unkVec = unkVec[6::].flatten()
     unkVecTrunc = T.set_subtensor(unkVecTrunc[0:], np.transpose(unkVec))
     a = Op(unkVecTrunc)
-    print(a)
     
-    
-    unkVecT = unkVecTrunc#theano.tensor.zeros(30)
+    unkVecT = unkVecTrunc
     _, receptor_data, cell_names_receptor = import_Rexpr()
     S = (OPgen(unkVecT, "T-reg", Op) / (OPgen(unkVecT, "T-reg", Op) + OPgen(unkVecT, "Naive Treg", Op) + OPgen(unkVecT, "Mem Treg", Op) + OPgen(unkVecT, "T-helper", Op) + OPgen(unkVecT, "Naive Th", Op) + OPgen(unkVecT, "Mem Th", Op) + OPgen(unkVecT, "NK", Op) + OPgen(unkVecT, "CD8+", Op) + OPgen(unkVecT, "Naive CD8+", Op) + OPgen(unkVecT, "Mem CD8+", Op)))
 
-    Sgrad = T.grad(S, unkVecT)
+    Sgrad = T.grad(S[0], unkVecT)
     
     #then need to set theano gradient
-    Sfunc = T.Function([unkVecT], Sgrad)
+    Sfunc = theano.function([unkVecT], Sgrad)
     
     #then need to execute using unkVec
     unkVec, _ = import_samples_2_15(N=1)
-    S_partials = Sfunc(S_partials)
+    S_partials = Sfunc(unkVec[6::].flatten())
+    print(S_partials)
     #plot
     y_pos = np.arange(nParams())
     plt.bar(y_pos, S_partials, ax = ax, align='center', alpha=1)
@@ -62,7 +61,7 @@ def OPgen(unkVec, CellTypes, Op):
     cell_names_receptor = cell_names_receptor.tolist()
     OpHolder = np.zeros(len(CellTypes))
     CellTypes = [CellTypes]
-    predVec = theano.tensor.zeros(1)
+    predVec = T.zeros(1)
 
     for i, Ctype in enumerate(CellTypes): #Update each vec for unique cell expression levels
         cell_data = receptor_data[cell_names_receptor.index(Ctype), :]
