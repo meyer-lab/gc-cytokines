4d53a1018fc62317aaa17cb1151ab77fd8e101bf
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2019-11-11 19:41:56 -0800

    All Partials Prelint

--- ckine/figures/figureS7.py
+++ ckine/figures/figureS7.py
@@ -6,7 +6,7 @@ import numpy as np
 import theano.tensor as T
 import theano
 from .figureCommon import subplotLabel, getSetup
-from ..model import getTotalActiveSpecies, receptor_expression, nParams
+from ..model import getTotalActiveSpecies, receptor_expression, nParams, getRateVec, getparamsdict, rxParams
 from ..imports import import_Rexpr, import_samples_2_15
 from ..differencing_op import runCkineDoseOp
 
@@ -14,7 +14,7 @@ from ..differencing_op import runCkineDoseOp
 def makeFigure():
     """Get a list of the axis objects and create a figure"""
     # Get list of axis objects
-    ax, f = getSetup((20, 4), (1, 1))
+    ax, f = getSetup((10, 4), (1, 1))
 
     for ii, item in enumerate(ax):
         subplotLabel(item, string.ascii_uppercase[ii])
@@ -27,7 +27,7 @@ def makeFigure():
 def Specificity(ax):
     """ Creates Theano Function for calculating Specificity gradient with respect to various parameters"""
     S_partials = Sfunc(unkVec.flatten()) / S.eval()
-    y_pos = np.arange(nParams() - 6)
+    y_pos = np.arange(rxParams() - 6)
     vars_string = ['kfwd', 'krev4', 'krev5', 'krev16', 'krev17', 'krev22', 'krev23', 'krev27', 'krev31', 'krev33', 'krev35', 'endo',
                    'Aendo', 'sortF', 'kRec', 'kDeg', 'Rexpr2Ra', 'Rexpr2Rb', 'RexprGC', 'Rexpr15Ra', 'Rexpr7Ra', 'Rexpr9R', 'Rexpr4Ra', 'Rexp21Ra']
     Derivs = ax.bar(y_pos, S_partials, width=1, align='center', alpha=1)
@@ -43,10 +43,10 @@ def OPgen(unkVecOP, CellTypes, OpC):
 
     for Ctype in CellTypes:  # Update each vec for unique cell expression levels
         cell_data = receptor_dataC[cell_names_receptorC.index(Ctype), :]
-        unkVecOP = T.set_subtensor(unkVecOP[16], receptor_expression(cell_data[0], unkVecOP[11], unkVecOP[14], unkVecOP[13], unkVecOP[15]))
-        unkVecOP = T.set_subtensor(unkVecOP[17], receptor_expression(cell_data[1], unkVecOP[11], unkVecOP[14], unkVecOP[13], unkVecOP[15]))
-        unkVecOP = T.set_subtensor(unkVecOP[18], receptor_expression(cell_data[2], unkVecOP[11], unkVecOP[14], unkVecOP[13], unkVecOP[15]))
-        unkVecOP = T.set_subtensor(unkVecOP[19], 0)
+        unkVecOP = T.set_subtensor(unkVecOP[46], receptor_expression(cell_data[0], unkVecOP[41], unkVecOP[44], unkVecOP[43], unkVecOP[45]))  # RA
+        unkVecOP = T.set_subtensor(unkVecOP[47], receptor_expression(cell_data[1], unkVecOP[41], unkVecOP[44], unkVecOP[43], unkVecOP[45]))  # Rb
+        unkVecOP = T.set_subtensor(unkVecOP[48], receptor_expression(cell_data[2], unkVecOP[41], unkVecOP[44], unkVecOP[43], unkVecOP[45]))  # Gc
+        unkVecOP = T.set_subtensor(unkVecOP[49], 0)  # 15
         Cell_Op = OpC(unkVecOP)
 
     return Cell_Op
@@ -54,6 +54,10 @@ def OPgen(unkVecOP, CellTypes, OpC):
 
 def barlabel(rects, labels, ax):
     """Attach a text label above each bar in *rects*, displaying its height."""
+    ODunk = np.zeros(60)
+    OD = getparamsdict(ODunk)
+    labels = list(OD.keys())
+    labels = labels[6::]
     for i, rect in enumerate(rects):
         height = rect.get_height()
         if height < 0:
@@ -62,17 +66,20 @@ def barlabel(rects, labels, ax):
                     xy=(rect.get_x() + rect.get_width() / 2, height),
                     xytext=(0, 3),  # 3 points vertical offset
                     textcoords="offset points",
-                    ha='center', va='bottom')
+                    ha='center', va='bottom',
+                    rotation=45)
 
 
 unkVec, _ = import_samples_2_15(N=1)
+unkVec = getRateVec(unkVec)
 CondIL = np.zeros((1, 6), dtype=np.float64)
 CondIL[0] = 1.
 Op = runCkineDoseOp(tt=np.array(500.), condense=getTotalActiveSpecies().astype(np.float64), conditions=CondIL)
-unkVecTrunc = T.zeros(24)
+unkVecTrunc = T.zeros(54)
 unkVec = unkVec[6::].flatten()
 unkVecTrunc = T.set_subtensor(unkVecTrunc[0:], np.transpose(unkVec))
 unkVecT = unkVecTrunc
+a = unkVecT.eval()
 _, receptor_data, cell_names_receptor = import_Rexpr()
 S = (OPgen(unkVecT, "T-reg", Op) /
      (OPgen(unkVecT, "T-reg", Op) +
