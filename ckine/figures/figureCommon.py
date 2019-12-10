95a3ab9fbb38266b501f40d35638c685112215f6
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2019-12-09 17:32:26 -0800

    Linted

--- ckine/figures/figureCommon.py
+++ ckine/figures/figureCommon.py
@@ -338,65 +338,12 @@ def import_pMuteins():
             # order of increasing time by cell type
             mutdata[4 * jj: 4 * (jj + 1), :] = np.flip(data[5 + (8 * jj): 9 + (8 * jj), 2 + 15 * ii: 14 + 15 * ii].astype(np.float) - zero_treatment, 0)
 
-    dataMeanM = pds.DataFrame(
-        {
-            'Cells': np.tile(
-                np.repeat(
-                    cell_names,
-                    48),
-                6),
-            'Ligand': np.concatenate(
-                (np.tile(
-                    np.array('IL2-060 monomeric'),
-                    384),
-                    np.tile(
-                    np.array("Cterm IL-2 monomeric WT"),
-                    384),
-                    np.tile(
-                    np.array("Cterm IL-2 monomeric V91K"),
-                    384),
-                    np.tile(
-                    np.array("IL2-109 monomeric"),
-                    384),
-                    np.tile(
-                    np.array("IL2-110 monomeric"),
-                    384),
-                    np.tile(
-                    np.array("Cterm N88D monomeric"),
-                    384))),
-            'Time': np.tile(
-                np.repeat(
-                    tps,
-                    12),
-                48),
-            'Concentration': np.tile(
-                ckineConc,
-                192),
-            'RFU': np.concatenate(
-                (IL60_mon_data.reshape(
-                    384,
-                ),
-                    Cterm_IL2_data.reshape(
-                    384,
-                ),
-                    Cterm_IL2_data_V91K.reshape(
-                    384,
-                ),
-                    IL2_109_data.reshape(
-                    384,
-                ),
-                    IL2_110_data.reshape(
-                    384,
-                ),
-                    Cterm_N88_mon_data.reshape(
-                    384,
-                ),
-                ))})
+    dataMeanM = pds.DataFrame({'Cells': np.tile(np.repeat(cell_names, 48), 6), 'Ligand': np.concatenate((np.tile(np.array('IL2-060 monomeric'), 384), np.tile(np.array("Cterm IL-2 monomeric WT"), 384), np.tile(np.array("Cterm IL-2 monomeric V91K"), 384), np.tile(np.array("IL2-109 monomeric"), 384), np.tile(np.array("IL2-110 monomeric"), 384), np.tile(np.array("Cterm N88D monomeric"), 384))), 'Time': np.tile(np.repeat(tps, 12), 48), 'Concentration': np.tile(ckineConc, 192), 'RFU': np.concatenate((IL60_mon_data.reshape(384, ), Cterm_IL2_data.reshape(384, ), Cterm_IL2_data_V91K.reshape(384, ), IL2_109_data.reshape(384, ), IL2_110_data.reshape(384, ), Cterm_N88_mon_data.reshape(384,  ), ))})
 
     return dataMeanM
 
 
-def calc_dose_response_mutein(unkVec, tps, muteinC, cell_receptors):
+def calc_dose_response_mutein(unkVec, input_params, tps, muteinC, mutein_name, cell_receptors):
     """ Calculates activity for a given cell type at various mutein concentrations and timepoints. """
 
     total_activity = np.zeros((len(muteinC), len(tps)))
