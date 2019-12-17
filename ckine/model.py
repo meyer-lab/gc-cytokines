df740891249d1209c5ba592ac0c2eb083412ba1e
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2019-12-16 18:15:30 -0800

    Improved

--- ckine/model.py
+++ ckine/model.py
@@ -331,17 +331,14 @@ def mut_adjust(rxntfr, mutdict, mut_name):
     """Adjust alpha beta and gamma affinities for muteins prior to run through model according to BLI data"""
     # Adjust a affinities
     input_params = mutdict.get(mut_name)
-    a_adjust = input_params[0] / rxntfr[0, 7]
     bg_adjust = input_params[1] / rxntfr[0, 10]
 
     # change for unkVec instead of dict
-    rxntfr[:, 7] = input_params[0] / rxntfr[:, 6]
-    #rxntfr[:, 7] = input_params[0] * rxntfr[:, 6]
+    rxntfr[:, 7] = input_params[0] * 0.6
     rxntfr[:, 27] = rxntfr[:, 7] * 5.0
 
-    # Adjust b/g affinities
+    #Adjust b/g affinities
     for ii in [8, 9, 10, 11, 12, 28, 29, 30, 31, 32]:
-        #rxntfr[:, ii] = rxntfr[:, ii] * bg_adjust
-        rxntfr[:, ii] = input_params[0] / rxntfr[0, 7]
-
+        rxntfr[:, ii] = rxntfr[:, ii] * bg_adjust
+        
     return rxntfr
