d277135e0127fe35932a733a81ef4c41eab49d0e
Aaron Meyer <ameyer@ucla.edu>
2019-12-30 10:43:47 -0800

    Try this.

--- ckine/fit.py
+++ ckine/fit.py
@@ -18,7 +18,7 @@ def load_data(filename):
 
 def sampling(M):
     """ This is the sampling that actually runs the model. """
-    return pm.sample(chains=2, model=M, tune=1000)
+    return pm.sample(init="advi+adapt_diag", chains=2, model=M)
 
 
 def commonTraf(trafficking=True):
