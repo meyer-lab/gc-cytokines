aea945ac6e320d9f10dac6bd8d56dd79626a4dda
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-02-04 16:43:02 -0800

    Update figure3.py

--- ckine/figures/figure3.py
+++ ckine/figures/figure3.py
@@ -27,7 +27,7 @@ for jj in range(4):
     factors = perform_decomposition(values, jj + 1)
     factors_activity.append(factors)
 logging.info("Decomposition finished.")
-n_pred_comps = 3  # Placed here to be also used by Figure 5
+n_pred_comps = 2  # Placed here to be also used by Figure 5
 
 
 def makeFigure():
@@ -62,7 +62,7 @@ def makeFigure():
     plot_timepoints(ax[6], tensor_time, tl.to_numpy(factors_activ[0]))
 
     plot_cells(ax[7], tl.to_numpy(factors_activ[1]), 1, 2, cell_names)
-    plot_cells(ax[8], tl.to_numpy(factors_activ[1]), 1, 3, cell_names)
+    plot_cells(ax[8], tl.to_numpy(factors_activ[1]), 1, 2, cell_names)
 
     legend = ax[7].get_legend()
     labels = (x.get_text() for x in legend.get_texts())
