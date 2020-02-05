88a0c1b92577ef240101c8919320577d23b53e8c
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-02-04 16:40:55 -0800

    Update figure6.py

--- ckine/figures/figure6.py
+++ ckine/figures/figure6.py
@@ -180,7 +180,7 @@ def Specificity(ax):
     colors = ["rich blue", "sun yellow"]
     sns.set_palette(sns.xkcd_palette(colors))
 
-    sns.catplot(data=df, x='rate', y='value', kind="bar", hue='cell', ax=ax, legend=False)
+    sns.barplot(data=df, x='rate', y='value', hue='cell', ax=ax, legend=False)
     ax.set_xlabel("")
     ax.legend()
     ax.set_yscale("symlog", linthreshy=0.01)
