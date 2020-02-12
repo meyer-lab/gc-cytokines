45ccb2c7c1e84dc545be18941e433c105588ba38
Brian Orcutt-Jahns <orcuttjahnsbrian@gmail.com>
2020-02-11 17:49:39 -0800

    Tweaks

--- ckine/figures/figureCommon.py
+++ ckine/figures/figureCommon.py
@@ -129,7 +129,7 @@ def plot_cells(ax, factors, component_x, component_y, cell_names, legend=True):
     ax.set_xlim(0, 1)
     ax.set_ylim(0, 1)
     if legend:
-        ax.legend(prop={"size": 7})
+        ax.legend(prop={"size": 6})
 
 
 def plot_ligand_comp(ax, factors, component_x, component_y, ligand_names):
@@ -479,12 +479,12 @@ def catplot_comparison(ax, df, legend=False, Mut=True):
     # plot predicted EC50
     sns.catplot(x="Cell Type", y="EC-50", hue="IL",
                 data=df.loc[(df['Time Point'] == 60.) & (df["Data Type"] == 'Predicted')],
-                legend=legend, legend_out=legend, ax=ax, marker='^', s=4)
+                legend=legend, legend_out=legend, ax=ax, marker='^', s=3.5)
 
     # plot experimental EC50
     sns.catplot(x="Cell Type", y="EC-50", hue="IL",
                 data=df.loc[(df['Time Point'] == 60.) & (df["Data Type"] == 'Experimental')],
-                legend=False, legend_out=False, ax=ax, marker='o', s=4)
+                legend=False, legend_out=False, ax=ax, marker='o', s=3.5)
 
     ax.set_xticklabels(ax.get_xticklabels(), rotation=40, fontsize=6.8, rotation_mode="anchor", ha="right")
     ax.set_xlabel("")  # remove "Cell Type" from xlabel
