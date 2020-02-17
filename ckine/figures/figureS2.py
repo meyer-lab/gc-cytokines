beb1511f24dd0b3775f08d4048d566f3581a9122
Aaron Meyer <aaron@asmlab.org>
2020-02-17 09:04:59 -0800

    Try this.

--- ckine/figures/figureS2.py
+++ ckine/figures/figureS2.py
@@ -30,26 +30,26 @@ def plot_geweke_2_15(ax, traf):
     trace = import_samples_2_15(Traf=traf, ret_trace=True)  # return the trace
 
     df = pd.DataFrame()
-    df[r'$k_{4}$'] = geweke(trace["rxn__0"])
-    df[r'$k_{5}$'] = geweke(trace["rxn__1"])
-    df[r'$k_{16}$'] = geweke(trace["rxn__2"])
-    df[r'$k_{17}$'] = geweke(trace["rxn__3"])
-    df[r'$k_{22}$'] = geweke(trace["rxn__4"])
-    df[r'$k_{23}$'] = geweke(trace["rxn__5"])
+    df[r'$k_{4}$'] = geweke(trace["rxn__0"])[:, 1]
+    df[r'$k_{5}$'] = geweke(trace["rxn__1"])[:, 1]
+    df[r'$k_{16}$'] = geweke(trace["rxn__2"])[:, 1]
+    df[r'$k_{17}$'] = geweke(trace["rxn__3"])[:, 1]
+    df[r'$k_{22}$'] = geweke(trace["rxn__4"])[:, 1]
+    df[r'$k_{23}$'] = geweke(trace["rxn__5"])[:, 1]
 
-    df['IL-2Rα'] = geweke(trace["Rexpr_2Ra__0"])
-    df['IL-2Rβ'] = geweke(trace["Rexpr_2Rb__0"])
-    df['IL-15Rα'] = geweke(trace["Rexpr_15Ra__0"])
-    df[r'$γ_{c}$'] = geweke(trace["Rexpr_gc__0"])
-    df[r'$C_{5}$'] = geweke(trace["scales__0"])
-    df[r'$k_{fwd}$'] = geweke(trace["kfwd__0"])
+    df['IL-2Rα'] = geweke(trace["Rexpr_2Ra__0"])[:, 1]
+    df['IL-2Rβ'] = geweke(trace["Rexpr_2Rb__0"])[:, 1]
+    df['IL-15Rα'] = geweke(trace["Rexpr_15Ra__0"])[:, 1]
+    df[r'$γ_{c}$'] = geweke(trace["Rexpr_gc__0"])[:, 1]
+    df[r'$C_{5}$'] = geweke(trace["scales__0"])[:, 1]
+    df[r'$k_{fwd}$'] = geweke(trace["kfwd__0"])[:, 1]
 
     if traf:  # add the trafficking parameters if necessary & set proper title
-        df[r'$k_{endo}$'] = geweke(trace["endo__0"])
-        df[r'$k_{endo,a}$'] = geweke(trace["activeEndo__0"])
-        df[r'$f_{sort}$'] = geweke(trace["sortF__0"])
-        df[r'$k_{rec}$'] = geweke(trace["kRec__0"])
-        df[r'$k_{deg}$'] = geweke(trace["kDeg__0"])
+        df[r'$k_{endo}$'] = geweke(trace["endo__0"])[:, 1]
+        df[r'$k_{endo,a}$'] = geweke(trace["activeEndo__0"])[:, 1]
+        df[r'$f_{sort}$'] = geweke(trace["sortF__0"])[:, 1]
+        df[r'$k_{rec}$'] = geweke(trace["kRec__0"])[:, 1]
+        df[r'$k_{deg}$'] = geweke(trace["kDeg__0"])[:, 1]
         ax.set_title(r'IL-2/-15 trafficking model')
     else:
         ax.set_title(r'IL-2/-15 no trafficking model')
@@ -72,16 +72,16 @@ def plot_geweke_4_7(ax):
     trace = import_samples_4_7(ret_trace=True)  # return the trace
 
     df = pd.DataFrame()
-    df[r'$k_{27}$'] = geweke(trace["k27rev__0"])
-    df[r'$k_{33}$'] = geweke(trace["k33rev__0"])
-    df[r'$C_{5}$'] = geweke(trace["scales__0"])
-    df[r'$C_{6}$'] = geweke(trace["scales__1"])
-    df[r'$k_{fwd}$'] = geweke(trace["kfwd__0"])
-    df[r'$k_{endo}$'] = geweke(trace["endo__0"])
-    df[r'$k_{endo,a}$'] = geweke(trace["activeEndo__0"])
-    df[r'$f_{sort}$'] = geweke(trace["sortF__0"])
-    df[r'$k_{rec}$'] = geweke(trace["kRec__0"])
-    df[r'$k_{deg}$'] = geweke(trace["kDeg__0"])
+    df[r'$k_{27}$'] = geweke(trace["k27rev__0"])[:, 1]
+    df[r'$k_{33}$'] = geweke(trace["k33rev__0"])[:, 1]
+    df[r'$C_{5}$'] = geweke(trace["scales__0"])[:, 1]
+    df[r'$C_{6}$'] = geweke(trace["scales__1"])[:, 1]
+    df[r'$k_{fwd}$'] = geweke(trace["kfwd__0"])[:, 1]
+    df[r'$k_{endo}$'] = geweke(trace["endo__0"])[:, 1]
+    df[r'$k_{endo,a}$'] = geweke(trace["activeEndo__0"])[:, 1]
+    df[r'$f_{sort}$'] = geweke(trace["sortF__0"])[:, 1]
+    df[r'$k_{rec}$'] = geweke(trace["kRec__0"])[:, 1]
+    df[r'$k_{deg}$'] = geweke(trace["kDeg__0"])[:, 1]
     ax.set_title(r'IL-4/-7 trafficking model')
 
     sns.scatterplot(data=df, ax=ax)
