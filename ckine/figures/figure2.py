9fa79843e9ae7b9072fdef2706c9fdc721d926fa
Brian Orcutt-Jahns <46577511+borcuttjahns@users.noreply.github.com>
2020-03-12 17:40:02 -0700

    Fig edit (#146)
    * Fixes Made
    
    * Lint
    
    * Update figure6.py
    
    Co-authored-by: Aaron Meyer <2065146+aarmey@users.noreply.github.com>

--- ckine/figures/figure2.py
+++ ckine/figures/figure2.py
@@ -308,5 +308,5 @@ def relativeGC(ax, unkVec2, unkVec4):
     sns.set_palette(sns.xkcd_palette(["violet", "violet", "violet", "goldenrod", "goldenrod", "goldenrod", "blue", "lightblue"]))
 
     a = sns.violinplot(data=np.log10(df), ax=ax, linewidth=0, scale="width")
-    a.set_xticklabels(a.get_xticklabels(), rotation=25, rotation_mode="anchor", ha="right", fontsize=8, position=(0, 0.02))
+    a.set_xticklabels(a.get_xticklabels(), rotation=45, rotation_mode="anchor", ha="right", fontsize=8, position=(0, 0.02))
     a.set(title=r"Relative $\mathrm{γ_{c}}$ Affinity", ylabel=r"$\mathrm{log_{10}(K_{a})}$")
