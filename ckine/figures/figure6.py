a983e2c05e503d0e7c2f6c16ee9fd6abbabb5924
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-03-12 17:20:10 -0700

    Update figure6.py

--- ckine/figures/figure6.py
+++ ckine/figures/figure6.py
@@ -184,7 +184,7 @@ def Specificity(ax):
     sns.set_palette(sns.xkcd_palette(colors))
 
     sns.catplot(data=df, x='rate', y='value', kind="bar", hue='cell', ax=ax, legend=False)
-    ax.set_ylabel(r"$\frac{d[Specificity]}{d[Param]}$")
+    ax.set_ylabel("d[Specificity] / d[Param]")
     ax.set_xlabel("")
     ax.legend()
     ax.set_yscale("symlog", linthreshy=0.01)
