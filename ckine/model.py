3e5a2bb4a1180b6066c59fcf2d9135ef755c1841
Aaron Meyer <2065146+aarmey@users.noreply.github.com>
2020-11-15 10:30:25 -0800

    Update model.py

--- ckine/model.py
+++ ckine/model.py
@@ -166,7 +166,7 @@ def receptor_expression(receptor_abundance, endo, kRec, sortF, kDeg):
 
 def condenseSENV(sensVin):
     """ Condense sensitivities down into the old rxnRates format. """
-    sensVin[:, 7:27] += sensVin[:, 27:47] * 5.0
+    sensVin[:, 7:27] += sensVin[:, 27:47] * 20.0
     sensVin[:, 10] += 12.0 * sensVin[:, 11] / 1.5 + 63.0 * sensVin[:, 12] / 1.5
     sensV = sensVin[:, np.array([0, 1, 2, 3, 4, 5, 6, 9, 10, 15, 16, 17, 18, 20, 22, 24, 26, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59])]
 
@@ -200,7 +200,7 @@ def getparamsdict(rxntfr):
     rd["surf.k35rev"] = rxntfr[16]
 
     for ii in ("1", "2", "4", "5", "10", "11", "13", "14", "16", "17", "22", "23", "25", "27", "29", "31", "32", "33", "34", "35"):
-        rd["endo.k" + ii + "rev"] = rd["surf.k" + ii + "rev"] * 5.0
+        rd["endo.k" + ii + "rev"] = rd["surf.k" + ii + "rev"] * 20.0
 
     rd["endo"], rd["activeEndo"], rd["sortF"], rd["kRec"], rd["kDeg"] = tuple(rxntfr[17:22])
     rd["Rexpr_2Ra"], rd["Rexpr_2Rb"], rd["Rexpr_gc"], rd["Rexpr_15Ra"], rd["Rexpr_7R"], rd["Rexpr_9R"], rd["Rexpr_4Ra"], rd["Rexpr_21Ra"] = tuple(rxntfr[22:30])
